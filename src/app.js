b29e72f0edf72cc217d9ee18061f6d1708c227ec
Rufus Pollock <rufus.pollock@okfn.org>
2012-05-08 21:50:03 +0100

    [all][m]: refactor to use google docs directly (so live data) and completely rework and extend the summary.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,85 @@
+var censusDatasets = [
+      'Election Results (national)',
+      'Company Register',
+      'National Map (Low resolution: 1:250,000 or better)',
+      'Government Budget (National, high level, not detailed)',
+      'Government Spending (National, transactional level data)',
+      'Legislation (laws and statutes) - National',
+      'National Statistical Data (economic and demographic information)',
+      'National Postcode/ZIP database',
+      'Public Transport Timetables',
+      'Environmental Data on major sources of pollutants (e.g. location, emissions)'
+      ];
+var censusKeys = [
+  'Timestamp',
+  'Census Country',
+  'Dataset',
+  'Data Availability [Does the data exist?]',
+  'Data Availability [Is it in digital form?]',
+  'Data Availability [Is it machine readable? (E.g. spreadsheet not PDF)]',
+  'Data Availability [Available in bulk?  (Can you get the whole dataset easily)]',
+  'Data Availability [Is it publicly available, free of charge?]',
+  'Data Availability [Is it openly licensed? (as per the http://OpenDefinition.org/)]',
+  'Data Availability [Is it up to date?]',
+  'Location of Data Online',
+  'Date it became available',
+  'Details and comments',
+  'Your name (optional)',
+  'Link for you (optional)'
+  ];
+
+function gdocsMunge(str) {
+  return str.replace(/[^a-zA-Z0-9_.]/g, '').toLowerCase();
+}
+
 jQuery(document).ready(function($) {
-  $.getJSON('data/summary.json', summaryTable);
+  var url = 'https://docs.google.com/spreadsheet/ccc?key=0Aon3JiuouxLUdEVnbG5pUFlyUzBpVkFXbXJ2WWpGTUE#gid=0'
+  var dataset = new recline.Model.Dataset({
+      id: 'opendatacensus',
+      url: url
+    },
+    backend='gdoc'
+  );
+  dataset.fetch().done(function() {
+    dataset.query().done(function() {
+      $('.loading').hide();
+      var data = dataset.currentDocuments.toJSON();
+      var summary = getSummaryData(data);
+      summaryTable(summary);
+    });
+  });
 });
 
+function getSummaryData(data) {
+  var countries = {};
+  function makeDatasetDict () {
+    var _out = {};
+    _.each(censusDatasets, function(ds) {
+      _out[ds] = {
+        count: 0,
+        responses: [],
+        isopen: false
+      };
+    });
+    return _out;
+  }
+  _.each(data, function(row) {
+      countries[row['censuscountry']] = makeDatasetDict();
+  });
+  _.each(data, function(row) {
+    var c = row['censuscountry'];
+    var d = row['dataset'];
+    count = countries[c][d].count || 0;
+    countries[c][d]['count'] = count + 1;
+    countries[c][d].responses.push(row);
+  });
+  
+  var out = {
+      'datasets': censusDatasets,
+      'countries': countries
+      }
+  return out;
+}
 
 function summaryTable(data) {
   var table = $('.response-summary');
@@ -10,16 +88,61 @@ function summaryTable(data) {
     table.find('thead tr').append($('<th />').text(name));
   });
   var countries = data.countries;
-  countries['All other countries'] = {};
   _.each(countries, function(country, name) {
     var row = $('<tr />');
-    row.append($('<th />').text(name));
+    row.append($('<th />').text(name).addClass('country-name'));
     _.each(datasets, function(dataset) {
       var count = country[dataset] ? country[dataset].count : 0;
-      row.append($('<td />').text(count).addClass('count-' + count));
+      if (count) {
+        // just get first response
+        var summary = '';
+        var map = {
+          'Yes': 'Y',
+          'No': 'N',
+          'No ': 'N',
+          'Unsure': '?'
+        }
+        var isopen = true;
+        _.each(censusKeys.slice(3,9), function(key, idx) {
+          var response = country[dataset].responses[0];
+          var answer = response[gdocsMunge(key)];
+          if (answer != 'Yes') {
+            isopen = false;
+          }
+          summary += map[answer];
+        });
+        var $td = $('<td />').addClass('count-' + count);
+        $td.append('<a class="btn short-summary">' + summary + '</a>');
+        if (isopen) {
+          $td.append('<a href="http://opendefinition.org/okd/"><img src="http://assets.okfn.org/images/ok_buttons/od_80x15_blue.png" /></a>');
+        }
+        $td.find('.short-summary').click(function(e) {
+          if ($td.find('.cell-summary').length > 0) {
+            $td.find('.cell-summary').toggle();
+          } else {
+            $td.append(cellSummary(country, dataset));
+          }
+        });
+        row.append($td);
+      } else {
+        row.append($('<td />').text('No info').addClass('count-' + count));
+      }
     });
     table.find('tbody').append(row);
   });
+}
+
+function cellSummary(country, dataset) {
+  var resp = $('<table />').addClass('cell-summary').addClass('table').addClass('table-bordered');
+  _.each(censusKeys.slice(1), function(key) {
+    var response = country[dataset].responses[0];
+    var answer = response[gdocsMunge(key)];
+    var $tr = $('<tr />');
+    $tr.append($('<th />').text(key));
+    $tr.append($('<td />').text(answer));
+    resp.append($tr);
+  });
+  return $('<div />').append(resp).html();
 }
 
 function graphSummary() {
