81de8691c2b8d5631b61a44fedb4e25a40253cad
Michael Bauer <mihi@lo-res.org>
2012-09-07 17:45:16 +0200

    front page info

--- src/catalogs.js
+++ src/catalogs.js
@@ -15,23 +15,6 @@ function init() {
     });
   
   }
-function countup(element,to) {
-  var n=0;
-  element.html(n);
-  if (n<to) {
-    n++;
-    element.html(n);
-    setTimeout(function() {_countup(element,to)},10);
-    }
-  }
-function _countup(element,to) {
-  var n=element.html();
-  if (n<to) {
-    n++;
-    element.html(n);
-    setTimeout(function() {_countup(element,to)},10);
-    }
-  }
 
 function getSummary(data) {
   var summary={};
--- src/census.js
+++ src/census.js
@@ -89,23 +89,6 @@ jQuery(document).ready(function($) {
   });
 });
 
-function countup(element,to) {
-  var n=0;
-  element.html(n);
-  if (n<to) {
-    n++;
-    element.html(n);
-    setTimeout(function() {_countup(element,to)},10);
-    }
-  }
-function _countup(element,to) {
-  var n=element.html();
-  if (n<to) {
-    n++;
-    element.html(n);
-    setTimeout(function() {_countup(element,to)},10);
-    }
-  }
 function summaryTop(summary) {
   countup($("#nc"),summary.countries.length);
   countup($("#nr"),summary.total)
