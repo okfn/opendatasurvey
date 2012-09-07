9b040cda0b9c4890543309616ed1604deab8bc40
Michael Bauer <mihi@lo-res.org>
2012-09-07 15:05:13 +0200

    basic statistics on catalogs

--- src/catalogs.js
+++ src/catalogs.js
@@ -5,12 +5,64 @@ function init() {
   $.getJSON(dcjson,function(data) {
     dataset=new recline.Model.Dataset({records: data});
     dataset.query({size: dataset.recordCount}).done(function () {
+      $("div.loading").hide();
       var map=new recline.View.Map({model: dataset})
       $("#map").append(map.el);
       map.render();
+      var summary=getSummary(dataset);
+      showSummary(summary);
       });
     });
   
+  }
+function countup(element,to) {
+  var n=0;
+  element.html(n);
+  if (n<to) {
+    n++;
+    element.html(n);
+    setTimeout(function() {_countup(element,to)},10);
+    }
+  }
+function _countup(element,to) {
+  var n=element.html();
+  if (n<to) {
+    n++;
+    element.html(n);
+    setTimeout(function() {_countup(element,to)},10);
+    }
+  }
+
+function getSummary(data) {
+  var summary={};
+  summary.total=dataset.recordCount;
+  summary.active=0;
+  summary.local=0;
+  summary.regional=0;
+  summary.national=0;
+  _.each(dataset.records.toJSON(), function(r) {
+    if ($.inArray("level.local",r.tags)>=0) {
+      summary.local++;
+      }
+    if ($.inArray("level.regional",r.tags)>=0) {
+      summary.regional++;
+      }
+    if ($.inArray("level.national",r.tags)>=0) {
+      summary.national++;
+      }
+    if (r.state=="active") {
+      summary.active++;
+    }
+  });
+  return summary;
+};
+
+function showSummary(summary) {
+  console.log(summary);
+  countup($("#tds"),summary.total);
+  countup($("#localds"),summary.local);
+  countup($("#regionalds"),summary.regional);
+  countup($("#nationalds"),summary.national);
   }
 
 $(document).ready(init);  
