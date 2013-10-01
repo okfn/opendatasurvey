de96c003aabfab51187adc042d611388d509b5f7
mattfullerton <matt.fullerton@gmail.com>
2013-10-01 22:14:04 +0200

    Disable G8 page and remove top-bar link to it and catalogs

--- app.js
+++ app.js
@@ -339,13 +339,13 @@ app.post('/country/review/:submissionid', function(req, res) {
 });
 
 
-app.get('/g8', function(req, res) {
-  //model.load(function() { //Don't reload for the public
-  console.log(model.openQuestions);
-  res.render('g8/index.html', {info: model.data.g8, questions: model.openQuestions});
-
-  //});
-});
+//app.get('/g8', function(req, res) {
+//  //model.load(function() { //Don't reload for the public
+//  console.log(model.openQuestions);
+//  res.render('g8/index.html', {info: model.data.g8, questions: model.openQuestions});
+//
+//  //});
+//});
 
 //TODO: If these are re-enabled, we should pass in: questions: model.openQuestions, as done for g8 and countries
 // app.get('/city', function(req, res) {
