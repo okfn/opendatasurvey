b25abb4a98ff981e77ae5a359ae4b663087c6e9f
mattfullerton <matt.fullerton@gmail.com>
2013-10-22 23:23:23 +0200

    [#223] Fixes #223 by surrounding relevant routes with if statements and setting loggedin to being always false

--- app.js
+++ app.js
@@ -101,6 +101,7 @@ app.all('*', function(req, res, next) {
   }
   if (config.get('production:readonly') === true) {
     res.locals.readonly = true;
+    req.session.loggedin = false;
   }
   res.locals.error_messages = req.flash('error');
   res.locals.info_messages = req.flash('info');
@@ -143,30 +144,34 @@ app.get('/contributors', function(req, res) {
     });
 });
 
-app.get('/faq', function(req, res) {
-  var tmpl = env.getTemplate('_snippets/questions.html');
-  var questionInfo = tmpl.render({
-    questions: model.data.questions
-  });
-  var dataTmpl = env.getTemplate('_snippets/datasets.html');
-  var dataInfo = dataTmpl.render({
-    datasets: model.data.country.datasets
-  });
-  fs.readFile('templates/faq.md', 'utf8', function(err, text) {
-    var marked = require('marked');
-    var content = marked(text);
-    content = content.replace('{{questions}}', questionInfo);
-    content = content.replace('{{datasets}}', dataInfo);
-    res.render('base.html', {
-      content: content,
-      title: 'FAQ - Frequently Asked Questions'
+if (config.get('production:readonly') !== true) {
+  app.get('/faq', function(req, res) {
+    var tmpl = env.getTemplate('_snippets/questions.html');
+    var questionInfo = tmpl.render({
+      questions: model.data.questions
+    });
+    var dataTmpl = env.getTemplate('_snippets/datasets.html');
+    var dataInfo = dataTmpl.render({
+      datasets: model.data.country.datasets
+    });
+    fs.readFile('templates/faq.md', 'utf8', function(err, text) {
+      var marked = require('marked');
+      var content = marked(text);
+      content = content.replace('{{questions}}', questionInfo);
+      content = content.replace('{{datasets}}', dataInfo);
+      res.render('base.html', {
+        content: content,
+        title: 'FAQ - Frequently Asked Questions'
+      });
     });
   });
-});
+}
 
-app.get('/contribute', function(req, res) {
-  res.render('country/contribute.html', {places: model.countryList});
-});
+if (config.get('production:readonly') !== true) {
+  app.get('/contribute', function(req, res) {
+    res.render('country/contribute.html', {places: model.countryList});
+  });
+}
 
 app.get('/country', function(req, res) {
   res.render('country/index.html', {info: model.data.country, questions: model.openQuestions});
@@ -233,6 +238,7 @@ app.get('/country/dataset/:dataset', function(req, res) {
 
 });
 
+if (config.get('production:readonly') !== true) {
 app.get('/country/submit', function(req, res) {
   var datasets = [];
   var ynquestions = model.data.questions.slice(0,9);
@@ -248,169 +254,172 @@ app.get('/country/submit', function(req, res) {
     });
   }
 
-  // look up if there is an entry and if so we use it to prepopulate the form
-  if (prefill.dataset && prefill.place) {
-    model.backend.getEntry({
-        place: prefill.place,
-        dataset: prefill.dataset,
-        year: prefill.year || model.DEFAULT_YEAR
-      }, function(err, obj) {
-        // we allow query args to override entry values
-        // might be useful (e.g. if we started having form errors and redirecting here ...)
-        if (obj) { // we might have a got a 404 etc
-          prefill = _.extend(obj, prefill);
+    // look up if there is an entry and if so we use it to prepopulate the form
+    if (prefill.dataset && prefill.place) {
+      model.backend.getEntry({
+          place: prefill.place,
+          dataset: prefill.dataset,
+          year: prefill.year || model.DEFAULT_YEAR
+        }, function(err, obj) {
+          // we allow query args to override entry values
+          // might be useful (e.g. if we started having form errors and redirecting here ...)
+          if (obj) { // we might have a got a 404 etc
+            prefill = _.extend(obj, prefill);
+          }
+          render(prefill);
         }
-        render(prefill);
-      }
-    );
-  } else {
-    render(prefill);
-  }
-});
-
-app.post('/country/submit', function(req, res) {
-  model.backend.insertSubmission(req.body, function(err, obj) {
-    //TODO: Do flash messages properly
-    if (err) {
-      console.log(err);
-      var msg = 'There was an error! ' + err;
-      req.flash('error', msg);
+      );
     } else {
-      var msg = 'Thank-you for your submission which has been received. It will now be reviewed by an Editor before being published. It may take up to a few minutes for your submission to appear here and up to a few days for it be reviewed. Please be patient.'
-      req.flash('info', msg);
-    }
-    res.redirect('country/overview/' + req.body['place']);
-  });
-});
-
-app.get('/country/submission/:id', function(req, res) {
-  model.backend.getSubmission({submissionid: req.params.id}, function(err, obj) {
-    if (err) {
-      res.send(500, 'There was an rror: ' + err);
-    }
-    // TODO: do something properly ...
-    res.send('Your submission exists');
-  });
-});
-
-app.get('/country/submission/:id.json', function(req, res) {
-  model.backend.getSubmission({submissionid: req.params.id}, function(err, obj) {
-    if (err) {
-      res.json(500, { error: { message: 'There was an error: ' + err } });
-    }
-    res.json(obj);
-  });
-});
-
-//Compare & update page
-app.get('/country/review/:submissionid', function(req, res) {
-  if (!req.session.loggedin) {
-    res.redirect('/country/login/?next=' + encodeURIComponent(req.url));
-    return;
-  }
-
-  var ynquestions = model.data.questions.slice(0,9);
-
-  model.backend.getSubmission({submissionid: req.params.submissionid}, function(err, obj) {
-    if (err) {
-      res.send(500, 'There was an error ' + err);
-    } else if (!obj) {
-      res.send(404, 'There is no submission with id ' + req.params.submissionid);
-    } else {
-      // let's see if there was an entry
-      model.backend.getEntry(obj, function(err, entry) {
-        if (!entry) {
-          entry = {};
-        }
-        var dataset = _.find(model.data.country.datasets, function(d) {
-          return (d.id == obj.dataset);
-        });
-        res.render('country/review/index.html', {
-          info: model.data.country,
-          ynquestions: ynquestions,
-          subrecord: obj,
-          prefill: obj,
-          currrecord: entry,
-          dataset: dataset,
-          datasetfriendly: model.datasetNamesMap[obj.dataset],
-          currentYear: model.data.country.currentYear
-        });
-      });
+      render(prefill);
     }
   });
-});
 
-app.post('/country/review/:submissionid', function(req, res) {
-  if (!req.session.loggedin) {
-    res.send(401, 'You are not authorized to do this');
-    return;
-  }
-
-  model.backend.getSubmission({
-      submissionid: req.params.submissionid
-    }, function(err, submission) {
+  app.post('/country/submit', function(req, res) {
+    model.backend.insertSubmission(req.body, function(err, obj) {
+      //TODO: Do flash messages properly
       if (err) {
-        res.send(500, err);
-        return;
-      } else if (!submission) {
-        res.send(404, 'No submission found for that info');
-        return;
+        console.log(err);
+        var msg = 'There was an error! ' + err;
+        req.flash('error', msg);
       } else {
-        processSubmission(submission)
+        var msg = 'Thank-you for your submission which has been received. It will now be reviewed by an Editor before being published. It may take up to a few minutes for your submission to appear here and up to a few days for it be reviewed. Please be patient.'
+        req.flash('info', msg);
       }
-    }
-  );
+      res.redirect('country/overview/' + req.body['place']);
+    });
+  });
 
-  function processSubmission(submission) {
-    if ((req.body['submit']) === "Publish") {
-      model.backend.acceptSubmission(submission, req.body, function(err) {
+  app.get('/country/submission/:id', function(req, res) {
+    model.backend.getSubmission({submissionid: req.params.id}, function(err, obj) {
+      if (err) {
+        res.send(500, 'There was an rror: ' + err);
+      }
+      // TODO: do something properly ...
+      res.send('Your submission exists');
+    });
+  });
+
+  app.get('/country/submission/:id.json', function(req, res) {
+    model.backend.getSubmission({submissionid: req.params.id}, function(err, obj) {
+      if (err) {
+        res.json(500, { error: { message: 'There was an error: ' + err } });
+      }
+      res.json(obj);
+    });
+  });
+
+  //Compare & update page
+  app.get('/country/review/:submissionid', function(req, res) {
+    if (!req.session.loggedin) {
+      res.redirect('/country/login/?next=' + encodeURIComponent(req.url));
+      return;
+    }
+
+    var ynquestions = model.data.questions.slice(0,9);
+
+    model.backend.getSubmission({submissionid: req.params.submissionid}, function(err, obj) {
+      if (err) {
+        res.send(500, 'There was an error ' + err);
+      } else if (!obj) {
+        res.send(404, 'There is no submission with id ' + req.params.submissionid);
+      } else {
+        // let's see if there was an entry
+        model.backend.getEntry(obj, function(err, entry) {
+          if (!entry) {
+            entry = {};
+          }
+          var dataset = _.find(model.data.country.datasets, function(d) {
+            return (d.id == obj.dataset);
+          });
+          res.render('country/review/index.html', {
+            info: model.data.country,
+            ynquestions: ynquestions,
+            subrecord: obj,
+            prefill: obj,
+            currrecord: entry,
+            dataset: dataset,
+            datasetfriendly: model.datasetNamesMap[obj.dataset],
+            currentYear: model.data.country.currentYear
+          });
+        });
+      }
+    });
+  });
+
+  app.post('/country/review/:submissionid', function(req, res) {
+    if (!req.session.loggedin) {
+      res.send(401, 'You are not authorized to do this');
+      return;
+    }
+
+    model.backend.getSubmission({
+        submissionid: req.params.submissionid
+      }, function(err, submission) {
         if (err) {
           res.send(500, err);
+          return;
+        } else if (!submission) {
+          res.send(404, 'No submission found for that info');
+          return;
         } else {
-          var msg = "Submission processed and entered into the census.";
+          processSubmission(submission)
+        }
+      }
+    );
+
+    function processSubmission(submission) {
+      if ((req.body['submit']) === "Publish") {
+        model.backend.acceptSubmission(submission, req.body, function(err) {
+          if (err) {
+            res.send(500, err);
+          } else {
+            var msg = "Submission processed and entered into the census.";
+            req.flash('info', msg);
+            doneUpdating(req, res, submission);
+          }
+        });
+      } else if (req.body['submit'] === "Reject") {
+        submission.reviewresult = 'rejected';
+        //The only field we need from the form is the reviewer
+        submission.reviewer = req.body['reviewername'];
+        model.backend.markSubmissionAsReviewed(submission, function(err) {
+          var msg = "Submission marked as rejected. The entry has been archived and marked as rejected. It will take a few minutes for this table to update. Thank you!";
           req.flash('info', msg);
           doneUpdating(req, res, submission);
-        }
-      });
-    } else if (req.body['submit'] === "Reject") {
-      submission.reviewresult = 'rejected';
-      //The only field we need from the form is the reviewer
-      submission.reviewer = req.body['reviewername'];
-      model.backend.markSubmissionAsReviewed(submission, function(err) {
-        var msg = "Submission marked as rejected. The entry has been archived and marked as rejected. It will take a few minutes for this table to update. Thank you!";
-        req.flash('info', msg);
-        doneUpdating(req, res, submission);
+        });
+      }
+    }
+    function doneUpdating(req, res, submission) {
+      //Get latest data
+      model.load(function() {
+        res.redirect('country/overview/' + submission.place);
       });
     }
-  }
-  function doneUpdating(req, res, submission) {
-    //Get latest data
-    model.load(function() {
-      res.redirect('country/overview/' + submission.place);
-    });
-  }
-});
-
-//"Log In" page
-app.get('/country/login', function(req, res) {
-  res.render('country/login.html', {
-    places: model.data.countrysubmissions.places,
-    place: req.query.place,
-    next: req.query.next
   });
-});
+}
 
-app.get('/country/logout', function(req, res) {
-  if (req.session.loggedin) delete req.session.loggedin;
-  res.redirect('/country/');
-});
+if (config.get('production:readonly') !== true) {
+  //"Log In" page
+  app.get('/country/login', function(req, res) {
+    res.render('country/login.html', {
+      places: model.data.countrysubmissions.places,
+      place: req.query.place,
+      next: req.query.next
+    });
+  });
 
-app.post('/country/login', function(req, res) {
-  if (config.get('production:readonly') === true) {
-      res.send(500, 'This version of the site does not support logging in.');
-  }
-  else doLogin(req, res);
-});
+  app.get('/country/logout', function(req, res) {
+    if (req.session.loggedin) delete req.session.loggedin;
+    res.redirect('/country/');
+  });
+
+  app.post('/country/login', function(req, res) {
+    if (config.get('production:readonly') === true) {
+        res.send(500, 'This version of the site does not support logging in.');
+    }
+    else doLogin(req, res);
+  });
+}
 
 function checkPassword(password, expectedHash) {
   var sha1 = crypto.createHash('sha1');
