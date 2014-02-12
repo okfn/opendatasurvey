3308fcd75852e15550ce500ed0d8d3cc115067d7
Rufus Pollock <rufus.pollock@okfn.org>
2014-02-12 19:37:00 +0000

    [debug][xs]: log config at start if in debug mode.

--- run.js
+++ run.js
@@ -1,10 +1,14 @@
 var model = require('./lib/model').OpenDataCensus
   , app = require('./app').app
+  , config = require('./lib/config')
   ; 
 
 // Booting up
 // ========================================================
 
+if (config.get('debug')) {
+  console.log(config.get());
+}
 model.load(function(err) {
   if (err) {
     console.error('Failed to load dataset info');
