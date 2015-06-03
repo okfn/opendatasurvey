a1957a7d2d5d83a8971e788c1b900882962b03f1
Paul Walsh <paulywalsh@gmail.com>
2015-06-03 15:07:15 +0300

    stub file for loader test

---
+++ tests/loaders.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var assert = require('chai').assert;
+
+
+describe('', function(){
+
+  describe('get()', function(){
+    it('is a string', function(done){
+
+      // just for example. Not a good test.
+      assert.typeOf('example', 'string');
+      done();
+
+    });
+  });
+
+});
