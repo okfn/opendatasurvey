6871ffde6d616e1aeec98cf6573b8172e194f16a
Paul Walsh <paulywalsh@gmail.com>
2015-06-03 14:34:06 +0300

    stub out loaders

---
+++ loaders/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var getRegistry = function() {
+
+};
+
+var getConfig = function(site_id) {
+
+};
+
+var getPlaces = function(site_id) {
+
+};
+
+var getDatasets = function(site_id) {
+
+};
+
+var getQuestions = function(site_id) {
+
+};
+
+var _migrateDatabase = function() {
+
+};
+
+var _migrateUsers = function() {
+
+};
+
+module.exports = {
+  getRegistry: getRegistry,
+  getConfig: getConfig,
+  getPlaces: getPlaces,
+  getDatasets: getDatasets,
+  getQuestions: getQuestions,
+  _migrateDatabase: _migrateDatabase,
+  _migrateUsers: _migrateUsers
+};
