e70df16989cc6501fa000b58e4cc4ad9030a0ade
Roman Sachenko <roman.sachenko@da-14.com>
2015-06-10 20:43:48 +0300

    check_if_sub_domain_middleware_done

--- middlewares/subDomain.js
+++ middlewares/subDomain.js
@@ -1,12 +1,27 @@
+var Promise = require('bluebird');
+var model = require('../lib/model').OpenDataCensus;
+var models = require('../models');
+
 var subDomain = {
   checkIfSubDomainExists: function (req, res, next) {
     var subDomain = getSubDomain(req);
     if (subDomain) {
-      req.isSubDomainExists = true;
+      return checkIfSubDomainIsInDb(subDomain).spread(function (err, searchResult) {
+        if (err) {
+          req.isSubDomainExists = false;
+        } else {
+          if (searchResult) {
+            req.isSubDomainExists = true;
+          } else {
+            req.isSubDomainExists = false;
+          }
+        }
+        next();
+      });
     } else {
       req.isSubDomainExists = false;
+      next();
     }
-    next();
   }
 };
 
@@ -16,4 +31,14 @@ function getSubDomain(req) {
   var hostParts = host.split('.');
   return hostParts[0];
 }
+
+
+function checkIfSubDomainIsInDb(subDomain) {
+  var searchQuery = {where: {id: subDomain}};
+  return models.Registry.find(searchQuery).then(function (searchResult) {
+    var data = searchResult['dataValues'] || false;
+    return [false, data];
+  });
+}
+
 module.exports = subDomain;
