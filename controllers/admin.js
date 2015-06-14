0c36ee9194a014563c5b03274b67f1eb62ba796e
Paul Walsh <paulywalsh@gmail.com>
2015-06-15 00:22:33 +0300

    Greatly simplify the web handlers for re/loading.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,97 +1,38 @@
+'use strict';
+
 var loaders = require('../loaders');
 
-/*
- * reload entities functionality
- */
+var loaderFactory = function(site_id, loader, response) {
+  return loader(site_id).spread(function(error, data) {
+    if (error) {
+      response.send({'status': 'error', message: error});
+    }
+    response.send({'status': 'ok', message: 'ok'});
+  });
+};
 
-//show reload dashboard
 var dashboard = function (req, res) {
   res.render('dashboard.html');
 };
 
-
-var createReloadResultRepsonse = function (err, reloadResult) {
-  var response = false;
-  if (err) {
-    response = {status: 'error', message: err};
-  } else {
-    response = {status: 'ok', data: 'ok'};
-  }
-  return response;
-};
-/*
- * reload places
- */
-var loadPlaces = function (req, res) {
-  var params = {
-    subDomain: req.subDomain,
-    configUrl: req.registryConfig
-  };
-
-  return loaders.loadPlaces(params).spread(function (err, reloadResult) {
-    var reloadResponse = createReloadResultRepsonse(err, reloadResult);
-    res.send(reloadResponse);
-  });
-};
-
-/*
- * reload datasets
- */
-var loadDatasets = function (req, res) {
-  var params = {
-    subDomain: req.subDomain,
-    configUrl: req.registryConfig
-  };
-
-  return loaders.loadDatasets(params).spread(function (err, reloadResult) {
-    var reloadResponse = createReloadResultRepsonse(err, reloadResult);
-    res.send(reloadResponse);
-  });
-};
-
-/*
- * reload Questions
- */
-var loadQuestions = function (req, res) {
-  var params = {
-    subDomain: req.subDomain,
-    configUrl: req.registryConfig
-  };
-
-  return loaders.loadQuestions(params).spread(function (err, reloadResult) {
-    var reloadResponse = createReloadResultRepsonse(err, reloadResult);
-    res.send(reloadResponse);
-  });
-};
-
-/*
- * reload Registry
- */
 var loadRegistry = function (req, res) {
-  var params = {
-    subDomain: req.subDomain,
-    configUrl: req.registryConfig
-  };
-
-  return loaders.loadRegistry(params).spread(function (err, reloadResult) {
-    var reloadResponse = createReloadResultRepsonse(err, reloadResult);
-    res.send(reloadResponse);
-  });
+  return loaderFactory(req.params.domain, loaders.loadRegistry, res);
 };
 
-/*
- * reload Config
- */
 var loadConfig = function (req, res) {
-  var params = {
-    subDomain: req.subDomain,
-    configUrl: req.registryConfig
-  };
+  return loaderFactory(req.params.domain, loaders.loadConfig, res);
+};
 
-  return loaders.loadConfig(params).spread(function (err, reloadResult) {
-    var reloadResponse = createReloadResultRepsonse(err, reloadResult);
-    res.send(reloadResponse);
-  });
+var loadPlaces = function (req, res) {
+  return loaderFactory(req.params.domain, loaders.loadPlaces, res);
+};
+
+var loadDatasets = function (req, res) {
+  return loaderFactory(req.params.domain, loaders.loadDatasets, res);
+};
+
+var loadQuestions = function (req, res) {
+  return loaderFactory(req.params.domain, loaders.loadQuestions, res);
 };
 
 module.exports = {
