d743f88e1c5e29b97a46e4cadeec54acb2040493
nide <nenegoro@gmail.com>
2015-06-21 20:47:41 +0300

    Registry loader tested (#547)

--- census/models/index.js
+++ census/models/index.js
@@ -1,5 +1,4 @@
-'use strict';
-
+var _ = require('underscore');
 var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
@@ -7,7 +6,8 @@ var basename = path.basename(module.filename);
 var mixinsFile = path.basename('./mixins.js');
 var config = require('../config').get('database');
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
-var db = {};
+var testSequelize = new Sequelize(config.testDatabase, config.username, config.password, config);
+var db = {test: {}};
 
 fs
   .readdirSync(__dirname)
@@ -18,16 +18,21 @@ fs
   .forEach(function (file) {
     console.log(file);
     var model = sequelize['import'](path.join(__dirname, file));
+    var testModel = testSequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
+    db.test[testModel.name] = testModel;
   });
 
 Object.keys(db).forEach(function (modelName) {
   if ('associate' in db[modelName]) {
     db[modelName].associate(db);
+    db.test[modelName].associate(db.test);
   }
 });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+db.test.sequelize = testSequelize;
+db.test.Sequelize = Sequelize;
 
-module.exports = db;
+module.exports = typeof IN_TESTING != 'undefined' && IN_TESTING ? db.test : db;
--- tests/loaders.js
+++ tests/loaders.js
@@ -1,18 +1,34 @@
-'use strict';
+IN_TESTING = true;
 
+var _ = require('underscore');
 var assert = require('chai').assert;
+var config = require('../census/config');
+var loaders = require('../census/loaders');
+var models = require('../census/models');
+var spreadSheetHandler = require('../census/loaders/includes/spreadSheetHandler');
 
 
-describe('', function(){
-
-  describe('get()', function(){
-    it('is a string', function(done){
-
-      // just for example. Not a good test.
-      assert.typeOf('example', 'string');
+describe('Data loaded from spread sheet into DB', function(){
+  beforeEach(function(done) {
+    models.sequelize.sync().then(function () {
+      console.log('Synced!');
       done();
-
     });
   });
 
+  it('Registry', function(done) {
+    this.timeout(10000);
+
+    models.Registry.destroy({truncate: true}).then(function() {
+      var registryIDs;
+
+      spreadSheetHandler.parse(config.get('registryUrl') || false).spread(function (E, R) {
+        registryIDs = _.pluck(R, 'censusid');
+
+        loaders.loadRegistry('demo').spread(function(E, D) {
+          models.Registry.findAll().then(function(D) { assert.deepEqual(registryIDs, _.pluck(D, 'id')); done(); });
+        });
+      });
+    });
+  });
 });
