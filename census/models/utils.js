202f1869bb0640282a547663b2d8dafae4da7f90
Paul Walsh <paulywalsh@gmail.com>
2015-07-05 19:44:33 +0300

    Add method to translate set of translatale results.

--- census/models/utils.js
+++ census/models/utils.js
@@ -4,9 +4,11 @@ var _ = require('underscore');
 
 
 var loadModels = function(querysets) {
+
   return Promise.all(_(querysets).map(function(V, K) {
     return new Promise(function(RS, RJ) { V.then(function(D) { RS([K, D]); }); });
   })).then(function(V) { return _.object(V); });
+
 };
 
 
@@ -19,11 +21,25 @@ var siteQuery = function(req, byYear) {
   if (byYear && req.params.year) {
     whereParams.year = req.params.year || req.app.get('year');
   }
+
   return {where: whereParams};
+
+};
+
+
+var translateSet = function(req, results) {
+
+  _.each(results, function(result, index, list) {
+    list[index] = result.translated(req.locale);
+  });
+
+  return results;
+
 };
 
 
 module.exports = {
   loadModels: loadModels,
-  siteQuery: siteQuery
+  siteQuery: siteQuery,
+  translateSet: translateSet
 };
