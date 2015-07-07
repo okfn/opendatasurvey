350657100837b9adf7f1fa4945e2ca5a0bbda0c7
nide <nenegoro@gmail.com>
2015-07-07 20:00:34 +0300

    Getting most recent Submission for certain Entry (#542)

--- migrations/submissions.js
+++ migrations/submissions.js
@@ -2,41 +2,37 @@ var _ = require('underscore');
 var chalk = require('chalk');
 var csv = require('csv');
 var fs = require('fs');
-var fileData = fs.readFileSync(process.argv[2], {encoding: 'utf-8'});
+var submissionsData = fs.readFileSync(process.argv[2], {encoding: 'utf-8'});
+var entriesData = fs.readFileSync(process.argv[3], {encoding: 'utf-8'});
 var models = require('../census/models');
 var moment = require('moment');
 var Promise = require('bluebird');
 
 
-csv.parse(fileData, {columns: true}, function(ER, D) {
-  Promise.each(D, function(E) {
-      models.Entry.upsert({
-        id     : E.submissionid.toString(),
-        site   : E.censusid,
-        year   : moment(E.timestamp, moment.ISO_8601).year(),
-        place  : E.place,
-        dataset: E.dataset,
+csv.parse(submissionsData, {columns: true}, function(SER, SD) {
+  csv.parse(entriesData, {columns: true}, function(EER, ED) {
+    _.each(ED, function(E) {
+      // Find most recent Submission with same key properties as Entry. Merge Submission
+      // properties into empty properties of Entry
+      var recentSubmission = _.chain(SD)
+        .filter(function(S) {
+          var entryYear = E.timestamp ? moment(E.timestamp, moment.ISO_8601).year() : E.year;
+          var submissionYear = S.timestamp ? moment(S.timestamp, moment.ISO_8601).year() : S.year;
 
-        answers: _.chain([
-          'exists',
-          'digital',
-          'public',
-          'machinereadable',
-          'bulk',
-          'openlicense',
-          'uptodate',
-          'online',
-          'free'
-        ]).map(function(Q) { return [Q, E[Q]]; }).object().value(),
 
-        submissionNotes: E.details,
-        reviewed       : parseInt(E.reviewed) === 1,
-        reviewResult   : E.reviewresult === 'accepted',
-        reviewComments : E.reviewcomments,
-        details        : '',
-        is_current     : E.reviewresult === 'accepted'
-      });
-  })
-    .then(function() { console.log(chalk.green((D.length - 1) + ' submission(s) successfully loaded!')); })
-    .catch(function(E) { console.log(chalk.red('Error while loading data: ' + E)); });
+          return entryYear === submissionYear && S.reviewresult === 'accepted' && _.every(
+            ['censusid', 'place', 'dataset'],
+            function(F) { return E[F] === S[F]; }
+          );
+        })
+
+        .sortBy(function(S) {
+          // Sort by unix time
+          return S.timestamp ? moment(S.timestamp, moment.ISO_8601).format('X') : S.year;
+        })
+
+        .last()
+        .value();
+    });
+  });
 });
