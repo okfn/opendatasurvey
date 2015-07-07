baca85b59f637217f16675aa9aaf9a918d65f2dc
Paul Walsh <paulywalsh@gmail.com>
2015-07-07 08:51:51 +0300

    Cleanup loader utils. Remove google-spreadsheet dependency.

--- census/loaders/utils.js
+++ census/loaders/utils.js
@@ -3,24 +3,28 @@
 var Promise = require('bluebird');
 var request = require('request');
 var csv = require('csv');
-var _ = require('underscore');
 
 
 var spreadsheetParse = function (fileUrl) {
+
   return new Promise(function (resolve, reject) {
-    var formattedUrl = parseSpreadsheetUrl(fileUrl);
-    formattedUrl = getCsvUrlForGoogleSheet(fileUrl);
-    getCsvData(formattedUrl, function (err, result) {
+
+    var csvUrl = getCsvForGoogleSheet(fileUrl);
+
+    getDataAsCsv(csvUrl, function (err, result) {
       if (err) {
         resolve([err, false]);
       } else {
         resolve([false, result]);
       }
     });
+
   });
+
 };
 
-var getCsvData = function (url, cb) {
+var getDataAsCsv = function (url, cb) {
+
   request.get(url, function (err, res, body) {
     var output = [];
     var parser = csv.parse({
@@ -37,7 +41,7 @@ var getCsvData = function (url, cb) {
         cb(err, false);
       })
       .on('end', function () {
-        var result = mapParsedCsvData(output);
+        var result = _mapParsedCsvData(output);
         cb(false, result);
       });
 
@@ -47,9 +51,8 @@ var getCsvData = function (url, cb) {
 };
 
 
+var _mapParsedCsvData = function(parsedData) {
 
-
-var mapParsedCsvData = function(parsedData) {
   var result = [];
   var keys = [];
   for (var i = 0; i < parsedData.length; i++) {
@@ -69,96 +72,61 @@ var mapParsedCsvData = function(parsedData) {
   }
 
   return result;
+
 };
 
 
-// parse a google spreadsheet url returning key and sheet (index)
-var parseSpreadsheetUrl = function (url) {
-  var regex = /.*spreadsheet\/ccc?.*key=([^#?&+]+)[^#]*(#gid=([\d]+).*)?/,
-    out = {key: '', sheet: 0}
-  ;
+var getSheetParams = function (url) {
+
+  var newPattern = /.*spreadsheets\/d\/([^#?&+]+)\/edit[^#]*(#gid=([\d]+).*)?/,
+      oldPattern = /.*spreadsheet\/ccc?.*key=([^#?&+]+)[^#]*(#gid=([\d]+).*)?/,
+      out = {key: '', sheet: 0},
+      matches;
+
   if (!url) {
     return out;
   }
 
-  var matches = url.match(regex);
+  matches = url.match(newPattern) || url.match(oldPattern);
+  console.log('SHIT DAWG');
+  console.log(matches);
   if (!!matches) {
     out.key = matches[1];
     out.sheet = parseInt(matches[3]);
+
     if (isNaN(out.sheet)) {
       out.sheet = 0;
     }
+
   }
+
   return out;
+
 };
 
 
-var getCsvUrlForGoogleSheet = function (url) {
-  var info = parseSpreadsheetUrl(url);
-  // not a google spreadsheet url
-  // (may already be in csv format ... or o/w a url elsewhere)
+var getCsvForGoogleSheet = function (url) {
+
+  var info = getSheetParams(url);
+
   if (info.key.length === 0) {
+    // sheet may have already been a CSV...
     return url;
+
   } else {
-    return _getCsvForGoogleSheet(info);
+
+    return getCsvFromSheetParams(info);
+
   }
+
 };
 
 
-var getSpreadSheetPage = function (params) {
-  var result = false;
-  var key = params['key'];
-  var sheet = params['index'];
-  result = _getCsvForGoogleSheet({
-    key: key,
-    sheet: sheet
-  });
+var getCsvFromSheetParams = function(info) {
 
-  return result;
-};
-
-
-var _getCsvForGoogleSheet = function(info) {
   var tmpl = 'https://docs.google.com/spreadsheet/pub?key=KEY&single=true&gid=INDEX&output=csv';
-  return tmpl
-    .replace('KEY', info.key)
-    .replace('INDEX', info.sheet)
-    ;
-};
+  return tmpl.replace('KEY', info.key).replace('INDEX', info.sheet);
 
-
-// updates the value in config to be an array as a side effect
-var convertToArray = function(config, key) {
-  var val = config.get(key);
-  if (_.isString(val)) {
-    var array = val.trim().split(/[\s,]+/);
-    config.set(key, array);
-  }
-}
-
-var loadConfig = function (cb) {
-
-  var configSheetInfo = exports.parseSpreadsheetUrl(config.get('configUrl'));
-  var configCsvUrl = exports.getCsvUrlForGoogleSheet(config.get('configUrl'));
-
-  getCsvData(configCsvUrl, function (err, data) {
-    if (err) {
-      cb(err);
-      return
-    }
-    data.forEach(function (record) {
-      config.set(record.key, record.value);
-    });
-
-    ['about_page', 'overview_page', 'faq_page', 'submit_page', 'review_page',
-      'contribute_page', ].forEach(function (item) {
-      if (config.get(item)) {
-        var out = marked(config.get(item));
-        config.set(item, out);
-      }
-    });
-    cb(null);
-  });
 };
 
 
@@ -198,9 +166,9 @@ var cleanUpCommon = function (db, records) {
 
 module.exports = {
   spreadsheetParse: spreadsheetParse,
-  cleanUpCommon: cleanUpCommon,
-  loadConfig: loadConfig,
-  getCsvUrlForGoogleSheet: getCsvUrlForGoogleSheet,
-  parseSpreadsheetUrl: parseSpreadsheetUrl,
-  getCsvData: getCsvData
+  getCsvFromSheetParams: getCsvFromSheetParams,
+  getSheetParams: getSheetParams,
+  getCsvForGoogleSheet: getCsvForGoogleSheet,
+  getDataAsCsv: getDataAsCsv,
+  cleanUpCommon: cleanUpCommon
 };
