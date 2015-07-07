23bd5efe86a159b2bb20e5072ea1f9e5edcdff8c
nide <nenegoro@gmail.com>
2015-07-07 21:34:18 +0300

    Merging Submission into Entry (#542)

--- migrations/submissions.js
+++ migrations/submissions.js
@@ -8,12 +8,44 @@ var models = require('../census/models');
 var moment = require('moment');
 var Promise = require('bluebird');
 
+var questions = [
+  'exists',
+  'digital',
+  'public',
+  'machinereadable',
+  'bulk',
+  'openlicense',
+  'uptodate',
+  'online',
+  'free'
+];
+
 
 csv.parse(submissionsData, {columns: true}, function(SER, SD) {
+  // All Submissions get into DB with isCurrent === false
+  Promise.each(SD, function(S) {
+    models.Entry.upsert({
+      id             : S.submissionid.toString(),
+      site           : S.censusid,
+      year           : S.timestamp ? moment(S.timestamp, moment.ISO_8601).year() : S.year,
+      place          : S.place,
+      dataset        : S.dataset,
+      answers        : _.chain(questions).map(function(Q) { return [Q, S[Q]]; }).object().value(),
+      submissionNotes: S.details,
+      reviewed       : parseInt(S.reviewed) === 1,
+      reviewResult   : S.reviewresult === 'accepted',
+      reviewComments : S.reviewcomments,
+      details        : '',
+      is_current     : false
+    })
+      .catch(function(E) { console.log(chalk.red('Error while loading data: ' + E)); });
+  })
+    .catch(function(E) { console.log(chalk.red('Error while loading data: ' + E)); });
+
+  // Walk over Entries sheet and find a match for each one in Submissions sheet.
+  // Merge non-empty properties of Entry into Submission properties.
   csv.parse(entriesData, {columns: true}, function(EER, ED) {
-    _.each(ED, function(E) {
-      // Find most recent Submission with same key properties as Entry. Merge Submission
-      // properties into empty properties of Entry
+    Promise.each(ED, function(E) {
       var recentSubmission = _.chain(SD)
         .filter(function(S) {
           var entryYear = E.timestamp ? moment(E.timestamp, moment.ISO_8601).year() : E.year;
@@ -33,6 +65,32 @@ csv.parse(submissionsData, {columns: true}, function(SER, SD) {
 
         .last()
         .value();
-    });
+
+      if(!recentSubmission)
+        return false;
+
+      console.log(chalk.dim(recentSubmission.submissionid));
+
+      // Merge non-empty properties of Entry into Submissions properties and save to DB
+      // with isCurrent === true
+      models.Entry.upsert({
+        id: recentSubmission.submissionid.toString(),
+        site: E.censusid,
+
+        answers: _.chain(questions)
+          .map(function(Q) { return [Q, E[Q] || recentSubmission[Q]]; })
+          .object()
+          .value(),
+
+        submissionNotes: E.details || recentSubmission.details,
+        reviewed       : parseInt(E.reviewed) === 1,
+        reviewResult   : E.reviewresult === 'accepted',
+        reviewComments : E.reviewcomments || recentSubmission.reviewcomments,
+        details        : '',
+        is_current     : true
+      });
+    })
+      .then(function() { console.log(chalk.green((ED.length + SD.length - 2) + ' submission(s) successfully loaded!')); })
+      .catch(function(E) { console.log(chalk.red('Error while loading data: ' + E)); });
   });
 });
