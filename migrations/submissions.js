3ae8e4ee99ed6a1cf20e911288cd0d1732cb33f7
Paul Walsh <paulywalsh@gmail.com>
2015-07-10 12:31:28 +0300

    Fixes to migrate submissions script.

--- migrations/submissions.js
+++ migrations/submissions.js
@@ -1,13 +1,12 @@
-var _ = require('underscore');
-var chalk = require('chalk');
-var csv = require('csv');
+var _ = require('lodash');
 var fs = require('fs');
-var submissionsData = fs.readFileSync(process.argv[2], {encoding: 'utf-8'});
-var entriesData = fs.readFileSync(process.argv[3], {encoding: 'utf-8'});
 var models = require('../census/models');
 var moment = require('moment');
 var Promise = require('bluebird');
-
+var csv = require('csv');
+var uuid = require('node-uuid');
+var validator = require('validator');
+var submissionsData = fs.readFileSync(process.argv[2], {encoding: 'utf-8'});
 var questions = [
   'exists',
   'digital',
@@ -17,80 +16,120 @@ var questions = [
   'openlicense',
   'uptodate',
   'online',
-  'free'
+  'free',
+  'url',
+  'format',
+  'licenseurl',
+  'dateavailable',
+  'officialtitle',
+  'publisher',
+  'qualityinfo',
+  'qualitystructure',
+  'details'
 ];
 
-
-csv.parse(submissionsData, {columns: true}, function(SER, SD) {
-  // All Submissions get into DB with isCurrent === false
-  Promise.each(SD, function(S) {
-    models.Entry.upsert({
-      id             : S.submissionid.toString(),
-      site           : S.censusid,
-      year           : S.timestamp ? moment(S.timestamp, moment.ISO_8601).year() : S.year,
-      place          : S.place,
-      dataset        : S.dataset,
-      answers        : _.chain(questions).map(function(Q) { return [Q, S[Q]]; }).object().value(),
-      submissionNotes: S.details,
-      reviewed       : parseInt(S.reviewed) === 1,
-      reviewResult   : S.reviewresult === 'accepted',
-      reviewComments : S.reviewcomments,
-      details        : '',
-      isCurrent     : false
-    })
-      .catch(function(E) { console.log(chalk.red('Error while loading data: ' + E)); });
-  })
-    .catch(function(E) { console.log(chalk.red('Error while loading data: ' + E)); });
-
-  // Walk over Entries sheet and find a match for each one in Submissions sheet.
-  // Merge non-empty properties of Entry into Submission properties.
-  csv.parse(entriesData, {columns: true}, function(EER, ED) {
-    Promise.each(ED, function(E) {
-      var recentSubmission = _.chain(SD)
-        .filter(function(S) {
-          var entryYear = E.timestamp ? moment(E.timestamp, moment.ISO_8601).year() : E.year;
-          var submissionYear = S.timestamp ? moment(S.timestamp, moment.ISO_8601).year() : S.year;
+var anonymousUserId = '0e7c393e-71dd-4368-93a9-fcfff59f9fff';
+var userQuery = models.User.findAll();
 
 
-          return entryYear === submissionYear && S.reviewresult === 'accepted' && _.every(
-            ['censusid', 'place', 'dataset'],
-            function(F) { return E[F] === S[F]; }
-          );
-        })
+userQuery
+  .then(function(users) {
 
-        .sortBy(function(S) {
-          // Sort by unix time
-          return S.timestamp ? moment(S.timestamp, moment.ISO_8601).format('X') : S.year;
-        })
+    csv.parse(submissionsData, {columns: true}, function(R, D) {
 
-        .last()
-        .value();
+      console.log('Submission count:');
+      console.log(D.length);
 
-      if(!recentSubmission)
-        return false;
+      Promise.each(D, function(obj) {
 
-      console.log(chalk.dim(recentSubmission.submissionid));
+        var submissionData = {},
+            normalized_timestamp,
+            subId,
+            subName,
+            submitter,
+            revId,
+            revName,
+            reviewer,
+            revLookup;
 
-      // Merge non-empty properties of Entry into Submissions properties and save to DB
-      // with isCurrent === true
-      models.Entry.upsert({
-        id: recentSubmission.submissionid.toString(),
-        site: E.censusid,
+        // fix old ids that are ints as strings
+        if (!validator.isUUID(obj.submissionid, 4)) {
+          obj.submissionid = uuid.v4();
+        }
 
-        answers: _.chain(questions)
-          .map(function(Q) { return [Q, E[Q] || recentSubmission[Q]]; })
-          .object()
-          .value(),
+        // normalize timestamps
+        normalized_timestamp = moment(obj.timestamp.trim());
+        if (normalized_timestamp.format() === "Invalid date") {
+          normalized_timestamp = moment(obj.timestamp.trim(), 'DD/MM/YYYY HH:mm:ss');
+        }
+        obj.timestamp = normalized_timestamp.format();
 
-        submissionNotes: E.details || recentSubmission.details,
-        reviewed       : parseInt(E.reviewed) === 1,
-        reviewResult   : E.reviewresult === 'accepted',
-        reviewComments : E.reviewcomments || recentSubmission.reviewcomments,
-        details        : '',
-        isCurrent     : true
+        // assign year correctly to fix old issues with mistmatched submission timestamp + year
+        if (normalized_timestamp.year() <= 2013) {
+          obj.year = 2013;
+        } else {
+          obj.year = normalized_timestamp.year();
+        }
+
+        // manipulate the submitter and reviewer data so we can reference the User table
+        // submitter can be anonymous, and we created an anonymous user for this
+        // backwards compat of anonymous
+        // reviewer can be empty, as some submissions can immediately become entries.
+        // in this case, we set the reviewer to the same user as the submitter.
+        subId = obj.submitterid.split(':');
+        subName = obj.submitter.split(' ');
+        revId = obj.reviewerid.split(':');
+        revName = obj.reviewer.split(' ');
+
+        // resolve our info on submitters and reviewers into user objects
+        if (subId[1] === 'anonymous' || subId[0] === '') {
+          submitter = _.find(users, function(user) {return user.id === anonymousUserId;});
+        } else {
+          submitter = _.find(users, function(user) {return user.providers === {'google': subId[1]};});
+        }
+
+        if (!revId[0]) {
+          revLookup = subId[1];
+        } else {
+          revLookup = revId[1];
+        }
+
+        if (revLookup === 'anonymous' || revId[0] === '' && subId[0] === '') {
+          reviewer = _.find(users, function(user) {return user.id === anonymousUserId;});
+        } else {
+          reviewer = _.find(users, function(user) {return user.providers === {'google': revLookup};});
+        }
+
+        // now let's explicitly set the data we want to save
+        submissionData.id = obj.submissionid;
+        submissionData.site = obj.censusid;
+        submissionData.createdAt = obj.timestamp;
+        submissionData.year = obj.year;
+        submissionData.place = obj.place;
+        submissionData.dataset = obj.dataset;
+        submissionData.answers = _.chain(questions).map(function(Q) { return [Q, obj[Q]]; }).object().value();
+        submissionData.submissionNotes = obj.details;
+        submissionData.reviewed = parseInt(obj.reviewed) === 1;
+        submissionData.reviewResult = obj.reviewresult === 'accepted';
+        submissionData.reviewComments = obj.reviewcomments;
+        submissionData.details = obj.details;
+        submissionData.isCurrent = false;
+        submissionData.submitter = submitter;
+        submissionData.reviewer = reviewer;
+
+        return models.Entry.create(submissionData)
+          .then(function(entry) {
+
+            console.log('saved new entry');
+            console.log(entry.createdAt);
+            console.log(' --- ');
+
+          })
+          .catch(function(error) {console.log('error::::');console.log(error);});
+
       });
-    })
-      .then(function() { console.log(chalk.green((ED.length + SD.length - 2) + ' submission(s) successfully loaded!')); })
-      .catch(function(E) { console.log(chalk.red('Error while loading data: ' + E)); });
-  });
-});
+
+    });
+
+  })
+  .catch(function(error) {console.log(error);});
--- migrations/utils.js
+++
@@ -1,4 +0,0 @@
-// Get CSV row field value by name, according to array of available columns
-module.exports.CSVRow = function(object, columns) {
-	return function(field) { return object[columns.indexOf(field)]; }
-}
