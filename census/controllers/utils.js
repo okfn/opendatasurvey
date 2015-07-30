bf205e5258445c1d1519c213591e3b6067250fef
Vyacheslav Levit <dev@vlevit.org>
2015-07-30 10:43:30 +0300

    Implement the new logic for validating data. Issue #610

--- census/controllers/utils.js
+++ census/controllers/utils.js
@@ -1,40 +1,85 @@
-var validateData = function (req) {
+var _makeChoiceValidator = function(param) {
+  return function(req) {
+    req.checkBody(param, "You must make a valid choice").isChoice();
+  };
+};
+
+var _validators = {
+  exists: {
+    validate: _makeChoiceValidator("exists"),
+    dependants: ["digital", "public", "uptodate"],
+    optional: ["publisher"]
+  },
+  digital: {
+    validate: _makeChoiceValidator("digital"),
+    dependants: ["online", "machinereadable", "bulk"]
+  },
+  public: {
+    validate: _makeChoiceValidator("public"),
+    dependants: ["free"]
+  },
+  free: {
+    validate: _makeChoiceValidator("free"),
+    dependants: ["openlicense"]
+  },
+  online: {
+    validate: _makeChoiceValidator("online"),
+    dependants: ["url"]
+  },
+  openlicense: {
+    validate: _makeChoiceValidator("openlicense")
+  },
+  machinereadable: {
+    validate: _makeChoiceValidator("machinereadable"),
+    dependants: ["format"]
+  },
+  format: {
+    validate: function(req) {
+      req.checkBody("machinereadable", "You must specify the format of data").notEmpty();
+    }
+  },
+  bulk: {
+    validate: _makeChoiceValidator("bulk")
+  },
+  url: {
+    validate: function(req) {
+      req.checkBody("publisher", "You must specify a valid URL").isURL();
+    }
+  },
+  uptodate: {
+    validate: _makeChoiceValidator("uptodate")
+  }
+};
+
+var _validateQuestion = function (req, question) {
+  /**
+   * Validate the question. If the answer is positive validate all the
+   * question dependants recursively.
+   */
+  _validators[question].validate(req);
+  if (req.body[question] === "true" && _validators[question].dependants) {
+    _validators[question].dependants.forEach(function(dep) {
+      _validateQuestion(req, dep);
+    });
+  }
+};
+
+var validateData = function(req) {
   /**
    * Ensures validation data is submitted by checking the POST data on
    * req.body according to the declared validation logic.
    * Used for new data submissions, and revision proposals.
    */
+  var errors;
 
-  var errors,
-    exists;
+  req.checkBody("place", "You must select a Place").notEmpty();
+  req.checkBody("dataset", "You must select a Dataset").notEmpty();
 
-  // first check exists for a yes answer.
-  if (req.body.hasOwnProperty('exists') && req.body.exists === 'true') {
-    exists = true;
-  }
-
-  req.checkBody('place', 'You must select a Place').notEmpty();
-  req.checkBody('dataset', 'You must select a Dataset').notEmpty();
-  req.checkBody('exists', 'You must make a valid choice').isChoice();
-
-  if (exists) {
-
-    req.checkBody('digital', 'You must make a valid choice').isChoice();
-    req.checkBody('public', 'You must make a valid choice').isChoice();
-    req.checkBody('free', 'You must make a valid choice').isChoice();
-    req.checkBody('online', 'You must make a valid choice').isChoice();
-    req.checkBody('machinereadable', 'You must make a valid choice').isChoice();
-    req.checkBody('bulk', 'You must make a valid choice').isChoice();
-    req.checkBody('openlicense', 'You must make a valid choice').isChoice();
-    req.checkBody('uptodate', 'You must make a valid choice').isChoice();
-
-  }
-
+  _validateQuestion(req, "exists");
   errors = req.validationErrors();
 
   return errors;
 };
-
 
 module.exports = {
   validateData: validateData
