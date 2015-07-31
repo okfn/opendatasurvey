5dd7fdbc60bc36214d320984534b6a589550fdc8
Vyacheslav Levit <dev@vlevit.org>
2015-08-01 00:13:15 +0300

    Validating data, iteration 2. Issue #610

--- census/controllers/utils.js
+++ census/controllers/utils.js
@@ -1,67 +1,120 @@
-var _makeChoiceValidator = function(param) {
+var makeChoiceValidator = function(param) {
   return function(req) {
     req.checkBody(param, "You must make a valid choice").isChoice();
   };
 };
 
-var _validators = {
+var validators = {
   exists: {
-    validate: _makeChoiceValidator("exists"),
-    dependants: ["digital", "public", "uptodate"],
-    optional: ["publisher"]
+    validate: makeChoiceValidator("exists"),
+    require: ["digital", "public", "uptodate"]
   },
   digital: {
-    validate: _makeChoiceValidator("digital"),
-    dependants: ["online", "machinereadable", "bulk"]
+    validate: makeChoiceValidator("digital"),
+    require: ["online", "machinereadable", "bulk"],
+    expectFalse: ["online", "machinereadable", "bulk"]
   },
   public: {
-    validate: _makeChoiceValidator("public"),
-    dependants: ["free"]
+    validate: makeChoiceValidator("public"),
+    require: ["free"],
+    expectFalse: ["free", "online", "openlicense", "bulk"]
   },
   free: {
-    validate: _makeChoiceValidator("free"),
-    dependants: ["openlicense"]
+    validate: makeChoiceValidator("free"),
+    require: ["openlicense"],
+    expectFalse: ["openlicense"]
   },
   online: {
-    validate: _makeChoiceValidator("online"),
-    dependants: ["url"]
+    validate: makeChoiceValidator("online"),
+    require: ["url"]
   },
   openlicense: {
-    validate: _makeChoiceValidator("openlicense")
+    validate: makeChoiceValidator("openlicense")
   },
   machinereadable: {
-    validate: _makeChoiceValidator("machinereadable"),
-    dependants: ["format"]
+    validate: makeChoiceValidator("machinereadable"),
+    require: ["format"]
+  },
+  bulk: {
+    validate: makeChoiceValidator("bulk")
+  },
+  uptodate: {
+    validate: makeChoiceValidator("uptodate")
   },
   format: {
+    type: "string",
     validate: function(req) {
       req.checkBody("machinereadable", "You must specify the format of data").notEmpty();
     }
   },
-  bulk: {
-    validate: _makeChoiceValidator("bulk")
-  },
   url: {
+    type: "string",
     validate: function(req) {
-      req.checkBody("publisher", "You must specify a valid URL").isURL();
+      req.checkBody("url", "You must specify a valid URL").isURL();
     }
-  },
-  uptodate: {
-    validate: _makeChoiceValidator("uptodate")
   }
 };
 
-var _validateQuestion = function (req, question) {
+var validateQuestion = function(req, question, parentValue, validated) {
   /**
-   * Validate the question. If the answer is positive validate all the
-   * question dependants recursively.
+   * Validate the question.
+   *
+   * If the answer is positive ("true") validate the field with all
+   * possible values.
+   *
+   * If it's negative ("false" or "null"):
+   *
+   *  * check that all the "expectFalse" question values are "false".
+   *
+   *  * check all the required fields have the same values as its
+   *    parent ("false" or "null") unless the field's type is string,
+   *    in that case ensure that the string is empty.
+   *
+   * Iterate over the required questions recursively.
    */
-  _validators[question].validate(req);
-  if (req.body[question] === "true" && _validators[question].dependants) {
-    _validators[question].dependants.forEach(function(dep) {
-      _validateQuestion(req, dep);
+
+  var validator = validators[question],
+      value = req.body[question],
+      parentValue = parentValue || "true",
+      validated = validated || [];
+
+  // ensure false values for expectFalse questions
+  if (value === "false" && validator.expectFalse) {
+    validator.expectFalse.forEach(function(child) {
+      if (validated.indexOf(question) == -1) {
+        req.checkBody(child, "You can specify only 'false'").equals("false");
+        validated.push(child);
+      }
     });
   }
+
+  if (validated.indexOf(question) == -1) {
+    // not yet validated
+
+    // validate depending on the question value
+    if (parentValue === "null" || parentValue === "false") {
+      // validate falsy values
+      if (validator.type === "string") {
+        req.checkBody(question, "You must not specify this field").equals("");
+      } else {
+        req.checkBody(question, "You can specify only '" + parentValue + "'")
+           .equals(parentValue);
+      }
+    } else {
+      // parentValue has a truthy value, validate as normal
+      validator.validate(req);
+    }
+
+    validated.push(question);
+  }
+
+  // validate recursively
+  if (validator.require) {
+    validator.require.forEach(function(child) {
+      validateQuestion(req, child, value, validated);
+    });
+  }
+
 };
 
 var validateData = function(req) {
@@ -70,12 +123,15 @@ var validateData = function(req) {
    * req.body according to the declared validation logic.
    * Used for new data submissions, and revision proposals.
    */
-  var errors;
+  var errors,
+      validated,
+      unvalidated;
 
   req.checkBody("place", "You must select a Place").notEmpty();
   req.checkBody("dataset", "You must select a Dataset").notEmpty();
 
-  _validateQuestion(req, "exists");
+  validateQuestion(req, "exists");
+
   errors = req.validationErrors();
 
   return errors;
