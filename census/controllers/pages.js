122cf31cdc1aa77056612d5606c4276a325f55c8
Paul Walsh <paulywalsh@gmail.com>
2015-08-02 18:55:38 +0300

    Run the core data display views through a new data access layer.

--- census/controllers/pages.js
+++ census/controllers/pages.js
@@ -8,31 +8,23 @@ var Promise = require('bluebird');
 
 var faq = function (req, res) {
 
-  var qTmpl = req.app.get('view_env').getTemplate('_snippets/questions.html');
-  var dTmpl = req.app.get('view_env').getTemplate('_snippets/datasets.html');
-  var gettext = res.locals.gettext;
+  var qTmpl = req.app.get('view_env').getTemplate('_snippets/questions.html'),
+      dTmpl = req.app.get('view_env').getTemplate('_snippets/datasets.html'),
+      dataOptions = _.merge(modelUtils.getDataOptions(req), {with: {Entry: false, Place: false}}),
+      gettext = res.locals.gettext;
 
-  modelUtils.loadModels({
-
-    datasets: req.app.get('models').Dataset.findAll(modelUtils.siteQuery(req)),
-    questions: req.app.get('models').Question.findAll(modelUtils.siteQuery(req))
-
-  }).then(function(D) {
-
-    var qContent = qTmpl.render({gettext: gettext, questions: _.sortByOrder(D.questions, 'order', 'asc')});
-    var dContent = dTmpl.render({gettext: gettext, datasets: _.sortByOrder(D.datasets, 'order', 'asc')});
-    var mContent = req.params.site.settings.missing_place_html;
-    var content = marked(req.params.site.settings.faq_page)
-      .replace('{{questions}}', qContent)
-      .replace('{{datasets}}', dContent)
-      .replace('{{missing_place}}', mContent);
-
-    res.render('base.html', {
-      content: content,
-      title: 'FAQ - Frequently Asked Questions'
-    });
-
-  }).catch(console.log.bind(console));
+  modelUtils.getData(dataOptions)
+    .then(function(data) {
+      var qContent = qTmpl.render({gettext: gettext, questions: data.questions}),
+          dContent = dTmpl.render({gettext: gettext, datasets: data.datasets}),
+          mContent = req.params.site.settings.missing_place_html;
+      data.title = 'FAQ - Frequently Asked Questions';
+      data.content = marked(req.params.site.settings.faq_page)
+        .replace('{{questions}}', qContent)
+        .replace('{{datasets}}', dContent)
+        .replace('{{missing_place}}', mContent);
+      return res.render('base.html', data);
+    }).catch(console.log.bind(console));
 };
 
 
@@ -77,26 +69,18 @@ var changes = function (req, res) {
 
 
 var contribute = function (req, res) {
-
-  var content = marked(req.params.site.settings.contribute_page);
-
   res.render('base.html', {
-    content: content,
+    content: marked(req.params.site.settings.contribute_page),
     title: 'Contribute'
   });
-
 };
 
 
 var about = function (req, res) {
-
-  var content = marked(req.params.site.settings.about_page);
-
   res.render('base.html', {
-    content: content,
+    content: marked(req.params.site.settings.about_page),
     title: 'About'
   });
-
 };
 
 
@@ -118,285 +102,98 @@ var resultJson = function (req, res) {
 
 
 var overview = function (req, res) {
-  var byYear; if (!req.params.cascade) { byYear = true; }
-  var entryQueryParams = _.merge(modelUtils.siteQuery(req, byYear), {where: {isCurrent: true}, order: '"createdAt" DESC'});
-  var questionQueryParams = _.merge(modelUtils.siteQuery(req), {where: {type: ''}, order: 'score DESC'});
-  var datasetQueryParams = modelUtils.siteQuery(req);
 
-  modelUtils.loadModels({
+  /**
+   * An overview of data, optionally by year.
+   */
 
-    entries: req.app.get('models').Entry.findAll(entryQueryParams),
-    datasets: req.app.get('models').Dataset.findAll(datasetQueryParams),
-    places: req.app.get('models').Place.findAll(modelUtils.siteQuery(req)),
-    questions: req.app.get('models').Question.findAll(questionQueryParams)
-
-  }).then(function(D) {
-
-    var placeCount,
-        currentEntryCount,
-        currentEntryOpenCount,
-        openDataPercentCount,
-        openDataPercent,
-        urlContext;
-
-    if (req.params.cascade) {
-      urlContext = '';
-      D.entries = modelUtils.cascadeEntries(D.entries);
-    } else {
-      urlContext = '/YEAR'.replace('YEAR', req.params.year);
-    }
-
-    if (Array.isArray(D.entries)) {
-      currentEntryCount = D.entries.length;
-      currentEntryOpenCount = _.filter(D.entries, function(e) {return e.isOpen() === true;}).length;
-      openDataPercent = parseInt((currentEntryOpenCount / currentEntryCount) * 100, 10);
-
-      _.each(D.entries, function(e) {
-        e.computedYCount = e.yCount(D.questions);
-      });
-    } else {
-
-      currentEntryCount = 0;
-      currentEntryOpenCount = 0;
-      openDataPercentCount = 0;
-
-    }
-
-    if (Array.isArray(D.places)) {
-
-      placeCount = D.places.length;
-
-      _.each(D.places, function(p) {
-        p.computedScore = p.score(D.entries, D.questions);
-      });
-
-    } else {
-
-      placeCount = 0;
-
-    }
-
-    res.render('overview.html', {
-
-      submissionsAllowed: (req.params.year === req.app.get('year')),
-      placeCount: placeCount,
-      currentEntryCount: currentEntryCount,
-      currentEntryOpenCount: currentEntryOpenCount,
-      openDataPercent: openDataPercent,
-      extraWidth: D.datasets.length > 12,
-      customText: req.params.site.settings.overview_page,
-      missingPlaceText: req.params.site.settings.missing_place_html,
-      places: _.sortByOrder(modelUtils.translateSet(req, D.places), 'computedScore', 'desc'),
-      datasets: _.sortByOrder(modelUtils.translateSet(req, D.datasets), 'order', 'asc'),
-      questions: modelUtils.translateSet(req, D.questions),
-      entries: D.entries,
-      urlContext: urlContext
-    });
-  }).catch(console.log.bind(console));
+  modelUtils.getData(modelUtils.getDataOptions(req))
+    .then(function(data) {
+      data.urlContext = '';
+      if (!req.params.cascade) { data.urlContext += '/YEAR'.replace('YEAR', req.params.year); }
+      data.submissionsAllowed = (req.params.year === req.app.get('year'));
+      data.extraWidth = data.datasets.length > 12;
+      data.customText = req.params.site.settings.overview_page;
+      data.missingPlaceText = req.params.site.settings.missing_place_html;
+      return res.render('overview.html', data);
+    }).catch(console.log.bind(console));
 };
 
 
 var place = function (req, res) {
 
-  var byYear; if (!req.params.cascade) { byYear = true; }
-  var placeQueryParams = _.merge(modelUtils.siteQuery(req), {where: {id: req.params.place}});
-  var entryQueryParams = _.merge(modelUtils.siteQuery(req, byYear),
-                                 {where: {place: req.params.place},
-                                  order: '"updatedAt" DESC',
-                                  include: [{model: req.app.get('models').User, as: 'Submitter'},
-                                            {model: req.app.get('models').User, as: 'Reviewer'}]});
-  var questionQueryParams = _.merge(modelUtils.siteQuery(req), {where: {type: ''}, order: 'score DESC'});
+  /**
+   * An overview of places, optionally by year.
+   */
 
-  modelUtils.loadModels({
+  modelUtils.getData(modelUtils.getDataOptions(req))
+    .then(function(data) {
+      if (!data.place) {
+        return res.status(404)
+          .send('There is no matching place in our database. ' +
+                'Are you sure you have spelled it correctly? Please check the ' +
+                '<a href="/">overview page</a> for the list of places');
+      }
 
-    place: req.app.get('models').Place.findOne(placeQueryParams),
-    datasets: req.app.get('models').Dataset.findAll(modelUtils.siteQuery(req)),
-    questions: req.app.get('models').Question.findAll(questionQueryParams),
-    entries: req.app.get('models').Entry.findAll(entryQueryParams)
-
-  }).then(function(D) {
-
-    var reviewers = [],
-        submitters = [],
-        urlContext;
-
-    if (req.params.cascade) {
-      urlContext = '';
-      // not needed as happens in template with new query
-      // D.entries = modelUtils.cascadeEntries(D.entries);
-    } else {
-      urlContext = '/YEAR'.replace('YEAR', req.params.year);
-    }
-
-    if (!D.place) {
-
-      res.send(404, 'There is no place with ID ' + D.place.id + ' in our database. ' +
-               'Are you sure you have spelled it correctly? Please check the ' +
-               '<a href="/">overview page</a> for the list of places');
-      return;
-
-    }
-
-    if (Array.isArray(D.entries)) {
-
-      _.each(D.entries, function(e, i, l) {
-        e.computedYCount = e.yCount(D.questions);
-        reviewers.push(e.Reviewer);
-        submitters.push(e.Submitter);
-      });
-
-    }
-
-    D.place.computedScore = D.place.score(D.entries, D.questions);
-
-    res.render('place.html', {
-      entries: D.entries,
-      place: D.place.translated(req.locale),
-      questions: modelUtils.translateSet(req, D.questions),
-      datasets: _.sortByOrder(modelUtils.translateSet(req, D.datasets), 'order', 'asc'),
-      loggedin: req.session.loggedin,
-      year: req.params.year,
-      submissionsAllowed: (req.params.year === req.app.get('year')),
-      reviewers: _.uniq(reviewers, 'id'),
-      submitters: _.uniq(submitters, 'id'),
-      urlContext: urlContext
-    });
-
-  }).catch(console.log.bind(console));
+      data.urlContext = '';
+      if (!req.params.cascade) { data.urlContext += '/YEAR'.replace('YEAR', req.params.year); }
+      data.loggedin = req.session.loggedin;
+      data.year = req.params.year;
+      data.submissionsAllowed = (req.params.year === req.app.get('year'));
+      data.extraWidth = data.datasets.length > 12;
+      return res.render('place.html', data);
+    }).catch(console.log.bind(console));
 };
 
 
 var dataset = function (req, res) {
 
-  var byYear; if (!req.params.cascade) { byYear = true; }
-  var datasetQueryParams = _.merge(modelUtils.siteQuery(req), {where: {id: req.params.dataset}});
-  var entryQueryParams = _.merge(modelUtils.siteQuery(req, byYear),
-                                 {where: {dataset: req.params.dataset},
-                                  include: [{model: req.app.get('models').User, as: 'Submitter'},
-                                            {model: req.app.get('models').User, as: 'Reviewer'}],
-                                  order: '"createdAt" DESC'});
-  var questionQueryParams = _.merge(modelUtils.siteQuery(req), {where: {type: ''}, order: 'score DESC'});
+  /**
+   * An overview of datasets, optionally by year.
+   */
 
-  modelUtils.loadModels({
+  modelUtils.getData(modelUtils.getDataOptions(req))
+    .then(function(data) {
+      if (!data.dataset) {
+        return res.status(404)
+          .send('There is no matching dataset in our database. ' +
+                'Are you sure you have spelled it correctly? Please check the ' +
+                '<a href="/">overview page</a> for the list of places');
+      }
 
-    dataset: req.app.get('models').Dataset.findOne(datasetQueryParams),
-    places: req.app.get('models').Place.findAll(modelUtils.siteQuery(req)),
-    questions: req.app.get('models').Question.findAll(questionQueryParams),
-    entries: req.app.get('models').Entry.findAll(entryQueryParams)
-
-  }).then(function(D) {
-
-    var reviewers = [],
-        submitters = [],
-        currentEntries,
-        pendingEntries,
-        urlContext;
-
-    if (req.params.cascade) {
-      urlContext = '';
-      // not needed as happens in template with new query
-      // D.entries = modelUtils.cascadeEntries(D.entries);
-    } else {
-      urlContext = '/YEAR'.replace('YEAR', req.params.year);
-    }
-
-    if (!D.dataset) {
-
-      res.send(404, 'There is no dataset with ID ' + D.dataset.id + ' in our database. ' +
-               'Are you sure you have spelled it correctly? Please check the ' +
-               '<a href="/">overview page</a> for the list of datasets');
-      return;
-
-    }
-
-    if (Array.isArray(D.entries)) {
-
-      _.each(D.entries, function(e, i, l) {
-        e.computedYCount = e.yCount(D.questions);
-        reviewers.push(e.reviewer);
-        submitters.push(e.submitter);
-      });
-
-    }
-
-    if (Array.isArray(D.places)) {
-
-      _.each(D.places, function(p) {
-        p.computedScore = p.score(D.entries, D.questions);
-      });
-
-    }
-
-    currentEntries = _.where(D.entries, {'isCurrent': true});
-    pendingEntries = _.where(D.entries, {'isCurrent': false, 'reviewed': false});
-
-    res.render('dataset.html', {
-
-      currentEntries: _.sortByOrder(currentEntries, function(e) {return e.yCount(D.questions);}, 'desc'),
-      pendingEntries: _.sortByOrder(pendingEntries, function(e) {return e.yCount(D.questions);}, 'desc'),
-      dataset: D.dataset.translated(req.locale),
-      questions: modelUtils.translateSet(req, D.questions),
-      places: _.sortByOrder(modelUtils.translateSet(req, D.places), 'computedScore', 'desc'),
-      year: req.params.year,
-      submissionsAllowed: (req.params.year === req.app.get('year')),
-      urlContext: urlContext
-
-    });
-
-  }).catch(console.log.bind(console));
-
+      data.urlContext = '';
+      if (!req.params.cascade) { data.urlContext += '/YEAR'.replace('YEAR', req.params.year); }
+      data.loggedin = req.session.loggedin;
+      data.year = req.params.year;
+      data.submissionsAllowed = (req.params.year === req.app.get('year'));
+      return res.render('dataset.html', data);
+    }).catch(console.log.bind(console));
 };
 
 
 var entry = function (req, res) {
-  var byYear; if (!req.params.cascade) { byYear = true; }
-  var entryQueryParams = _.merge(modelUtils.siteQuery(req, byYear),
-                                 {where: {dataset: req.params.dataset, place: req.params.place, isCurrent: true},
-                                  include: [{model: req.app.get('models').User, as: 'Submitter'},
-                                            {model: req.app.get('models').User, as: 'Reviewer'}],
-                                  order: '"createdAt" DESC'});
-  var datasetQueryParams = _.merge(modelUtils.siteQuery(req), {where: {id: req.params.dataset}});
-  var placeQueryParams = _.merge(modelUtils.siteQuery(req), {where: {id: req.params.place}});
-  var questionQueryParams = _.merge(modelUtils.siteQuery(req), {order: 'score DESC'});
 
-  modelUtils.loadModels({
+  /**
+   * An overview of the current entry for a place/dataset, optionally by year.
+   */
 
-    entry: req.app.get('models').Entry.findOne(entryQueryParams),
-    place: req.app.get('models').Place.findOne(placeQueryParams),
-    dataset: req.app.get('models').Dataset.findOne(datasetQueryParams),
-    questions: req.app.get('models').Question.findAll(questionQueryParams)
+  var dataOptions = _.merge(modelUtils.getDataOptions(req), {ynQuestions: false});
 
-  }).then(function(D) {
+  modelUtils.getData(dataOptions)
+    .then(function(data) {
+      if (!data.entry) {
+        return res.status(404)
+          .send('There is no matching entry in our database. ' +
+                'Are you sure you have spelled it correctly? Please check the ' +
+                '<a href="/">overview page</a> for the list of places');
+      }
 
-    var urlContext;
-
-    if (req.params.cascade) {
-      urlContext = '';
-    } else {
-      urlContext = '/YEAR'.replace('YEAR', req.params.year);
-    }
-
-    if (!D.entry) {
-
-      res.send(404, 'There is no matching entry in our database. ' +
-               'Please check the <a href="/">overview page</a> for available entries.');
-      return;
-
-    }
-
-    res.render('entry.html', {
-
-      entry: D.entry,
-      place: D.place.translated(req.locale),
-      dataset: D.dataset.translated(req.locale),
-      questions: modelUtils.translateSet(req, D.questions),
-      year: req.params.year,
-      urlContext: urlContext
-
-    });
-
-  }).catch(console.log.bind(console));
-
+      data.urlContext = '';
+      if (!req.params.cascade) { data.urlContext += '/YEAR'.replace('YEAR', req.params.year); }
+      data.year = req.params.year;
+      data.submissionsAllowed = (req.params.year === req.app.get('year'));
+      return res.render('entry.html', data);
+    }).catch(console.log.bind(console));
 };
 
 
--- census/models/utils.js
+++ census/models/utils.js
@@ -3,51 +3,272 @@
 var _ = require('lodash');
 
 
-var loadModels = function(querysets) {
-
+var loadModels = function(querysets, options) {
   return Promise.all(_.map(querysets, function(V, K) {
     return new Promise(function(RS, RJ) { V.then(function(D) { RS([K, D]); }); });
-  })).then(function(V) { return _.object(V); });
-
+  })).then(function(V) { return {data: _.object(V), options: options}; });
 };
 
 
-var siteQuery = function(req, byYear) {
+var siteQuery = function(domain, year, byYear) {
+  var whereParams = {site: domain};
+  if (byYear && year) { whereParams.year = year; }
+  return {where: whereParams};
+};
 
-  var whereParams = {
-    site: req.params.domain
-  };
 
-  if (byYear && req.params.year) {
-    whereParams.year = req.params.year;
+var translateSet = function(locale, results) {
+  _.each(results, function(result, index, list) {
+    list[index] = result.translated(locale);
+  });
+  return results;
+};
+
+
+var queryData = function(options) {
+
+  /**
+   * Query the database for data.
+   * if options.ynQuestions, then only get yn
+   * options.models has models
+   * options.with.{MODELNAME} to control queries actually made. can be done better.
+   */
+  var entryParams = _.merge(siteQuery(options.domain, options.year, !options.cascade),
+                            {
+                              order: '"updatedAt" DESC',
+                              include: [
+                                {model: options.models.User, as: 'Submitter'},
+                                {model: options.models.User, as: 'Reviewer'}
+                              ]
+                            }),
+      datasetParams = _.merge(siteQuery(options.domain), {order: '"order" ASC'}),
+      placeParams = _.merge(siteQuery(options.domain), {order: 'id ASC'}),
+      questionParams = _.merge(siteQuery(options.domain), {order: 'score DESC'}),
+      querysets = {};
+
+  if (options.ynQuestions) { questionParams =  _.merge(questionParams, {where: {type: ''}}); }
+
+  // prep the querysets object
+  if (options.place) {
+    placeParams = _.merge(placeParams, {where: {id: options.place}});
+    entryParams = _.merge(entryParams, {where: {place: options.place}});
+    if (options.with.Place) { querysets.place = options.models.Place.findOne(placeParams); }
+  } else {
+    if (options.with.Place) { querysets.places = options.models.Place.findAll(placeParams); }
   }
 
-  return {where: whereParams};
+  if (options.dataset) {
+    datasetParams = _.merge(datasetParams, {where: {id: options.dataset}});
+    entryParams = _.merge(entryParams, {where: {dataset: options.dataset}});
+    if (options.with.Dataset) { querysets.dataset = options.models.Dataset.findOne(datasetParams); }
+  } else {
+    if (options.with.Dataset) { querysets.datasets = options.models.Dataset.findAll(datasetParams); }
+  }
 
+  if (options.place && options.dataset) {
+    entryParams = _.merge(entryParams, {where: {isCurrent: true}});
+    if (options.with.Entry) { querysets.entry = options.models.Entry.findOne(entryParams); }
+  } else {
+    if (options.with.Entry) { querysets.entries = options.models.Entry.findAll(entryParams); }
+  }
+
+  if (options.with.Question) { querysets.questions = options.models.Question.findAll(questionParams); }
+
+  return loadModels(querysets, options);
 };
 
 
-var translateSet = function(req, results) {
+var processStats = function(data, options) {
 
-  _.each(results, function(result, index, list) {
-    list[index] = result.translated(req.locale);
-  });
+  /**
+   * Process all data for stats.
+   */
 
-  return results;
+  data.stats = {};
 
+  if (Array.isArray(data.entries)) {
+    data.stats.currentEntryCount = data.entries.length;
+    data.stats.currentEntryOpenCount = _.filter(data.entries, function(e) { return e.isOpen() === true; }).length;
+    data.stats.openDataPercent = parseInt((data.stats.currentEntryOpenCount / data.stats.currentEntryCount) * 100, 10);
+  } else {
+    data.stats.currentEntryCount = 0;
+    data.stats.currentEntryOpenCount = 0;
+    data.stats.openDataPercentCount = 0;
+  }
+
+  if (Array.isArray(data.places)) {
+    data.stats.placeCount = data.places.length;
+  } else {
+    data.stats.placeCount = 0;
+  }
+
+  return data;
 };
 
 
 var cascadeEntries = function(entries) {
   var cascaded = [];
   var grouped = _.groupBy(entries, function(e) { return e.place + '/' + e.dataset; });
-
-  _.each(grouped, function(value, key) {
-    if (value) {
-      cascaded.push(_.sortByOrder(value, ['year', 'updatedAt'], 'desc')[0]);
-    }
+  _.each(grouped, function(value) {
+    if (value) { cascaded.push(_.sortByOrder(value, ['year', 'updatedAt'], 'desc')[0]); }
   });
   return cascaded;
+};
+
+
+var setEntryUrl = function(entry) {
+      if (entry.isCurrent) {
+        entry.url = '/entry/PLACE/DATASET'
+          .replace('PLACE', entry.place)
+          .replace('DATASET', entry.dataset);
+      } else {
+        entry.url = '/census/submission/ID'.replace('ID', entry.id);
+      }
+};
+
+
+// var deref = function (type, entry, data) {
+//   if (type === 'place') {
+//     if (data.place) {
+//       return data.place;
+//     } else {
+//       console.log('?????');
+//       return _.find(data.places, function(p) { return p.id === entry.place; });
+//     }
+//   } else if (type === 'dataset') {
+//     if (data.dataset) {
+//       return data.dataset;
+//     } else {
+//       return _.find(data.datasets, function(d) { return d.id === entry.dataset; });
+//     }
+//   }
+//   return null;
+// };
+
+
+var processEntries = function(data, options) {
+
+  /**
+   * Process the raw entries query.
+   */
+
+  if (data.entry) {
+    // do nothing. But, still need its related pending submissions?
+  } else {
+    if (Array.isArray(data.entries)) {
+      data.reviewers = [];
+      data.submitters = [];
+      _.each(data.entries, function(e) {
+        e.computedYCount = e.yCount(data.questions);
+        e.url = setEntryUrl(e);
+        // e.place = deref('place', e, data);
+        // e.dataset = deref('dataset', e, data);
+        data.pending = _.where(data.entries, {'isCurrent': false, 'reviewed': false});
+        data.reviewers.push(e.Reviewer);
+        data.submitters.push(e.Submitter);
+      });
+
+      _.remove(data.entries, function(e) { return e.isCurrent === false; });
+      data.reviewers = _.uniq(data.reviewers, 'id');
+      data.submitters = _.uniq(data.submitters, 'id');
+      // TODO: sort by e.yCount desc ??? was in dataset.html ...
+      if (options.cascade) { data.entries = cascadeEntries(data.entries); } // TODO: this is slower here, instead of at top of this stanza. but, logic has to change.
+
+    }
+  }
+
+  return data;
+};
+
+
+var processPlaces = function(data, options) {
+
+  /**
+   * Process the raw places query.
+   */
+
+  if (data.place) {
+    data.place = data.place.translated(options.locale);
+  } else {
+    if (Array.isArray(data.entries)) {
+      _.each(data.places, function(p) {
+        p.computedScore = p.score(data.entries, data.questions);
+      });
+      data.places = _.sortByOrder(translateSet(options.locale, data.places), 'computedScore', 'desc');
+    }
+  }
+
+  return data;
+};
+
+
+var processDatasets = function(data, options) {
+
+  /**
+   * Process the raw datasets query.
+   */
+
+  if (data.dataset) {
+    data.dataset = data.dataset.translated(options.locale);
+  } else {
+    data.datasets = translateSet(options.locale, data.datasets);
+  }
+
+  return data;
+};
+
+
+var processQuestions = function(data, options) {
+
+  /**
+   * Process the raw questions query.
+   */
+
+  data.questions = translateSet(options.locale, data.questions);
+
+  return data;
+};
+
+
+var processData = function (result) {
+
+  /**
+   * Process the raw query data.
+   */
+  var data = result.data,
+      options = result.options;
+  if (data.entries || data.entry) { data = processEntries(data, options); }
+  if (data.places || data.place) { data = processPlaces(data, options); }
+  if (data.datasets || data.dataset) { data = processDatasets(data, options); }
+  if (data.questions) { data = processQuestions(data, options); }
+  data = processStats(data, options);
+
+  return data;
+};
+
+
+var getData = function(options) {
+
+  /**
+   * The interface to get data, all clean and ready like.
+   */
+
+  return queryData(options).then(processData);
+};
+
+
+var getDataOptions = function(req) {
+  return {
+    models: req.app.get('models'),
+    domain: req.params.domain,
+    dataset: req.params.dataset,
+    place: req.params.place,
+    year: req.params.year,
+    cascade: req.params.cascade,
+    ynQuestions: true,
+    locale: req.params.locale,
+    with: {Entry: true, Dataset: true, Place: true, Question: true}
+  };
 };
 
 
@@ -55,5 +276,7 @@ module.exports = {
   loadModels: loadModels,
   siteQuery: siteQuery,
   translateSet: translateSet,
-  cascadeEntries: cascadeEntries
+  cascadeEntries: cascadeEntries,
+  getDataOptions: getDataOptions,
+  getData: getData
 };
