5176f1a17f2a0c89953001ef9e7637c6b445a3ef
Sam Smith <sam@mintcanary.com>
2015-08-13 13:39:22 +0100

    add icons to popover

--- census/public/src/census.js
+++ census/public/src/census.js
@@ -53,7 +53,8 @@ $(document).ready(function($) {
           },
           content: function(){
             return OpenDataCensus.popoverBody(answers, details, url, actionurl, actiontext, submissions, submissionslength, year);
-          }
+          },
+          template: '<div class="popover overview-popup"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>'
         });
     });
 
