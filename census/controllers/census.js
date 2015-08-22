ac5af8569a4a3be94eef90de91338f6628b70a44
Paul Walsh <paulywalsh@gmail.com>
2015-08-22 08:52:08 +0300

    Fix bug introduced in #647. Need to get reviewers before checking if user is a reviewer.

--- census/controllers/census.js
+++ census/controllers/census.js
@@ -246,10 +246,11 @@ var reviewPost = function (req, res) {
 
     var dataOptions = _.merge(modelUtils.getDataOptions(req),
                               {place: result.place, dataset: result.dataset,
-                               cascade: true, with: {Dataset: false, Place: false, Question: false}});
+                               cascade: true, with: {Question: false}});
     modelUtils.getData(dataOptions)
       .then(function(data) {
 
+        data.reviewers = utils.getReviewers(req, data);
         if (!utils.canReview(data.reviewers, req.user)) {
           res.status(403).send('You are not allowed to review this entry');
           return;
