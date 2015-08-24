d987ada1767671a9f13a97fb9e7f6ae0d41ce835
Vyacheslav Levit <dev@vlevit.org>
2015-08-24 10:23:56 +0300

    Expand notification_log migration with first entry creation. Issue #608

--- census/migrations/20150730193000-notificationlog.js
+++ census/migrations/20150730193000-notificationlog.js
@@ -5,6 +5,7 @@ var path = require('path');
 var _ = require('lodash');
 var Promise = require('bluebird');
 var models = require('../models');
+var uuid = require('node-uuid');
 
 
 module.exports = {
@@ -24,15 +25,24 @@ module.exports = {
         allowNull: false,
         comment: 'Unique name for notification'
       },
-      
+
       lastAt: {
         type: Sequelize.DATE,
         allowNull: true,
         comment: 'Datetime notification last sent'
       }
+    }).then(function() {
+      queryInterface.sequelize.query(queryInterface.QueryGenerator.bulkInsertQuery('notification_log', [
+        {
+          id: uuid.v4(),
+          type: "comments",
+          lastAt: new Date()
+        }]));
     });
 
   },
 
-  down: function(queryInterface, Sequelize) { }
+  down: function(queryInterface, Sequelize) {
+    queryInterface.dropTable('notification_log');
+  }
 };
