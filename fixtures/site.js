3f6b103d920572ad0a3e75da777fc1c5809d9219
Levko Kravets <kravets@qarea.com>
2015-10-16 17:44:49 +0300

    #552 tests/app.js completely rewritten using Zombie

--- fixtures/site.js
+++ fixtures/site.js
@@ -6,7 +6,11 @@ var objects = [
       settings: {
         'about_page': 'About page very unique string 123',
         'faq_page': 'Faq page another unique string with **mark down** 777',
-        reviewers: ['email1@example.com', 'email3@example.com']
+        'overview_page': 'Lorem ipsum for overview page on Site1',
+        reviewers: ['email1@example.com', 'email3@example.com'],
+        'custom_footer': '<span style="display: none;">Custom footer</span>',
+        'custom_css': '/* Custom CSS rules */',
+        'navbar_logo': '<span style="display: none;">Custom navbar logo</span>'
       }
     }
   },
@@ -19,7 +23,7 @@ var objects = [
         reviewers: ['email2@example.com', 'email4@example.com'],
         'submit_page': '~~Submit~~ *page* unique **content**',
         'review_page': '*This* is **review** test',
-        locales: ['es', 'en', 'uk']
+        locales: ['en', 'es', 'uk']
       }
     }
   }
