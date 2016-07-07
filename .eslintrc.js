8703f3c0499c07baf39a5d9c0aad4a41d6f8cb65
Brook Elgie <specialbrew@gmail.com>
2016-07-07 13:01:45 +0100

    Add eslint to project and initial config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "jquery": true,
+        "es6": true,
+        "mocha": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": 0,
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ]
+    }
+};
