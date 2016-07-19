6f2797f4d204442fe501497e6260cc84c26c78a5
Brook Elgie <specialbrew@gmail.com>
2016-07-19 15:05:08 +0100

    Lint and ES6ify server files.

--- debug-server.js
+++ debug-server.js
@@ -1,4 +1,6 @@
 'use strict';
 
-var start = require('./census/app').start;
-start().then((result) => { console.log('SERVER STARTED'); });
+const start = require('./census/app').start;
+start().then(result => {
+  console.log('SERVER STARTED');
+});
