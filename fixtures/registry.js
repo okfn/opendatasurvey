f10da33b6ce11662871d55e3dd754decce13855c
Brook Elgie <specialbrew@gmail.com>
2016-07-31 17:32:39 +0100

    [#755] Registry fixture points to Test3 config
    The sites defined in the test Registry was pointing to the demo config.
    This is being used for an actual demo site, so isn't appropriate for
    testing purposes. A copy of demo config has been made (test 3) and this
    commit changes the configurl settings to point to it.

--- fixtures/registry.js
+++ fixtures/registry.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// configurl points to Test Site3 config.
 var objects = [
   {
     model: 'Registry',
@@ -5,7 +8,7 @@ var objects = [
       id: 'site1',
       settings: {
         adminemail: ['email1@example.com'],
-        configurl: "https://docs.google.com/a/okfn.org/spreadsheet/ccc?key=1QvZFGyICiuZmRxVll6peXkND_6QmHl7IQ_BYCw5Sso4&usp=sharing#gid=0"
+        configurl: 'https://docs.google.com/a/okfn.org/spreadsheet/ccc?key=1PoS2loqokUbwxMEFXb1Zoe3goLISGga9RumvNf-VxGo&usp=sharing#gid=0'
       }
     }
   },
@@ -15,7 +18,7 @@ var objects = [
       id: 'site2',
       settings: {
         adminemail: ['email2@example.com'],
-        configurl: "https://docs.google.com/a/okfn.org/spreadsheet/ccc?key=1QvZFGyICiuZmRxVll6peXkND_6QmHl7IQ_BYCw5Sso4&usp=sharing#gid=0"
+        configurl: 'https://docs.google.com/a/okfn.org/spreadsheet/ccc?key=1PoS2loqokUbwxMEFXb1Zoe3goLISGga9RumvNf-VxGo&usp=sharing#gid=0'
       }
     }
   }
