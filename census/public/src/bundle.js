ee4c1a65f91d28cde2054d117ea683b0d2761681
Brook Elgie <specialbrew@gmail.com>
2016-08-18 12:05:52 +0100

    [#753] Get initial data from embedded vars in html

--- census/public/src/bundle.js
+++ census/public/src/bundle.js
@@ -61,11 +61,8 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	// initial static data for development
-	// eslint-disable-next-line
-	var questions = [{ 'id': 'like_apples', 'text': 'Do you like apples?', 'type': '' }, { 'id': 'bananas_instead', 'text': 'Do you like bananas instead?', 'type': '' }, { 'id': 'apple_colour', 'text': 'Do you like *RED* apples?', 'type': '' }, { 'id': 'red_apple_today', 'text': 'Have you eaten a red apple today?', 'type': '' }, { 'id': 'doctor_away', 'text': 'Did it keep the doctor away?', 'type': '' }];
-	// eslint-disable-next-line
-	var qsSchema = [{ 'defaultProperties': { 'enabled': true, 'required': true, 'visible': true }, 'id': 'like_apples', 'position': 1 }, { 'defaultProperties': { 'enabled': false, 'required': false, 'visible': false }, 'id': 'bananas_instead', 'if': [{ 'providerId': 'like_apples', 'properties': { 'enabled': true, 'required': true, 'visible': true }, 'value': 'No' }], 'position': 1.1 }, { 'defaultProperties': { 'enabled': false, 'required': false, 'visible': true }, 'id': 'apple_colour', 'if': [{ 'providerId': 'like_apples', 'properties': { 'enabled': true, 'required': true }, 'value': 'Yes' }], 'position': 2 }, { 'defaultProperties': { 'enabled': false, 'required': false, 'visible': true }, 'id': 'red_apple_today', 'if': [{ 'providerId': 'apple_colour', 'properties': { 'enabled': true, 'required': true }, 'value': 'Yes' }], 'position': 3 }, { 'defaultProperties': { 'enabled': false, 'required': false, 'visible': false }, 'id': 'doctor_away', 'if': [{ 'providerId': 'red_apple_today', 'properties': { 'enabled': true, 'visible': true }, 'value': 'Yes' }], 'position': 3.1 }];
+	var qsSchema = window.qsSchema;
+	var questions = window.questions;
 	
 	(0, _reactDom.render)(_react2.default.createElement(_QuestionForm2.default, { questions: questions, qsSchema: qsSchema }), document.getElementById('questions'));
 
