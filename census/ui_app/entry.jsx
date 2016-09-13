ff4a81ae235a87558919499e683f4948ccb9ccf5
Brook Elgie <specialbrew@gmail.com>
2016-09-13 15:25:27 +0100

    [#753] Add dataset and place fields to form.
    Changing these submit fields will reload the form page with a new
    QuestionSet. The design of these will likely change to better
    communicate which Place/Dataset is the focus of the form, and to prevent
    accidental change.

--- census/ui_app/entry.jsx
+++ census/ui_app/entry.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from 'react'; // eslint-disable-line no-unused-vars
 import {render} from 'react-dom';
 import QuestionForm from './QuestionForm.jsx';
+import $ from 'jquery';
 
 let qsSchema = window.qsSchema;
 let questions = window.questions;
 
-// Add preliminary QuestionSet here, section A.
+$(function() {
+  // Reload page with Dataset QuestionSet if dataset is changed.
+  $('#dataset-select, #place-select').change(function(e) {
+    e.preventDefault();
+    let form = $(this).parents('form:first');
+    form.submit();
+  });
+});
 
 // Main QuestionSet, section B.
-render(<QuestionForm questions={questions} qsSchema={qsSchema} labelPrefix={'B'} />,
+render(<QuestionForm questions={questions}
+                     qsSchema={qsSchema}
+                     labelPrefix={'B'} />,
        document.getElementById('questions'));
