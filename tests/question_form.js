20a17fb53adf116b1964d54552cc853ab4a664ce
Brook Elgie <specialbrew@gmail.com>
2016-09-22 12:31:45 +0100

    [#753] Test for Question types rendering
    Each QuestionField* type will render in a QuestionForm component as
    defined in the passed `questions` and `qsSchema` objects.

--- tests/question_form.js
+++ tests/question_form.js
@@ -1,129 +1,168 @@
 'use strict';
 
+/* eslint-disable max-len */
+
 require('jsdom-global')();
+const _ = require('lodash');
 const React = require('react'); // eslint-disable-line no-unused-vars
 const mount = require('enzyme').mount;
 const expect = require('chai').expect;
 const QuestionForm = require('../census/ui_app/QuestionForm');
 
-let qsSchema = [
-  {
-    defaultProperties: {
-      enabled: true,
-      required: true,
-      visible: true
-    },
-    id: 'like_apples',
-    position: 1
-  },
-  {
-    defaultProperties: {
-      enabled: false,
-      required: false,
-      visible: false
-    },
-    id: 'bananas_instead',
-    if: [
-      {
-        providerId: 'like_apples',
-        properties: {
-          enabled: true,
-          required: true,
-          visible: true
-        },
-        value: 'No'
-      }
-    ],
-    position: 1.1
-  },
-  {
-    defaultProperties: {
-      enabled: false,
-      required: false,
-      visible: true
-    },
-    id: 'apple_colour',
-    if: [
-      {
-        providerId: 'like_apples',
-        properties: {
-          enabled: true,
-          required: true
-        },
-        value: 'Yes'
-      }
-    ],
-    position: 2
-  },
-  {
-    defaultProperties: {
-      enabled: false,
-      required: false,
-      visible: true
-    },
-    id: 'red_apple_today',
-    if: [
-      {
-        providerId: 'apple_colour',
-        properties: {
-          enabled: true,
-          required: true
-        },
-        value: 'Yes'
-      }
-    ],
-    position: 3
-  },
-  {
-    defaultProperties: {
-      enabled: false,
-      required: false,
-      visible: false
-    },
-    id: 'doctor_away',
-    if: [
-      {
-        providerId: 'red_apple_today',
-        properties: {
-          enabled: true,
-          visible: true
-        },
-        value: 'Yes'
-      }
-    ],
-    position: 3.1
-  }
-];
-
-let questions = [
-  {
-    id: 'like_apples',
-    text: 'Do you like apples?',
-    type: 'yesno'
-  },
-  {
-    id: 'bananas_instead',
-    text: 'Do you like bananas instead?',
-    type: 'yesno'
-  },
-  {
-    id: 'apple_colour',
-    text: 'Do you like *RED* apples?',
-    type: 'yesno'
-  },
-  {
-    id: 'red_apple_today',
-    text: 'Have you eaten a red apple today?',
-    type: 'yesno'
-  },
-  {
-    id: 'doctor_away',
-    text: 'Did it keep the doctor away?',
-    type: 'yesno'
-  }
-];
-
 describe('<QuestionForm />', () => {
+  describe('QuestionForm renders each question types', function() {
+    beforeEach(function () {
+      this.baseQSSchema = [
+        {
+          defaultProperties: {
+            enabled: true,
+            required: true,
+            visible: true
+          },
+          id: 'yesno_question',
+          position: 1
+        }
+      ];
+      this.baseQuestions = [
+        {
+          id: 'yesno_question',
+          text: 'Is this a yesno question?',
+          type: 'yesno'
+        }
+      ];
+    });
+
+    it('renders QuestionFieldYesNo type', function() {
+      this.wrapper =
+        mount(<QuestionForm questions={this.baseQuestions} qsSchema={this.baseQSSchema} />);
+      expect(this.wrapper.find('QuestionFieldYesNo')).to.have.length(1);
+    });
+    it('renders QuestionFieldText type', function() {
+      // Replace `type` in baseQuestions.
+      this.baseQuestions[0] = _.assign(this.baseQuestions[0], {type: 'text'});
+      this.wrapper =
+        mount(<QuestionForm questions={this.baseQuestions} qsSchema={this.baseQSSchema} />);
+      expect(this.wrapper.find('QuestionFieldText')).to.have.length(1);
+    });
+  });
+
+  let qsSchema = [
+    {
+      defaultProperties: {
+        enabled: true,
+        required: true,
+        visible: true
+      },
+      id: 'like_apples',
+      position: 1
+    },
+    {
+      defaultProperties: {
+        enabled: false,
+        required: false,
+        visible: false
+      },
+      id: 'bananas_instead',
+      if: [
+        {
+          providerId: 'like_apples',
+          properties: {
+            enabled: true,
+            required: true,
+            visible: true
+          },
+          value: 'No'
+        }
+      ],
+      position: 1.1
+    },
+    {
+      defaultProperties: {
+        enabled: false,
+        required: false,
+        visible: true
+      },
+      id: 'apple_colour',
+      if: [
+        {
+          providerId: 'like_apples',
+          properties: {
+            enabled: true,
+            required: true
+          },
+          value: 'Yes'
+        }
+      ],
+      position: 2
+    },
+    {
+      defaultProperties: {
+        enabled: false,
+        required: false,
+        visible: true
+      },
+      id: 'red_apple_today',
+      if: [
+        {
+          providerId: 'apple_colour',
+          properties: {
+            enabled: true,
+            required: true
+          },
+          value: 'Yes'
+        }
+      ],
+      position: 3
+    },
+    {
+      defaultProperties: {
+        enabled: false,
+        required: false,
+        visible: false
+      },
+      id: 'doctor_away',
+      if: [
+        {
+          providerId: 'red_apple_today',
+          properties: {
+            enabled: true,
+            visible: true
+          },
+          value: 'Yes'
+        }
+      ],
+      position: 3.1
+    }
+  ];
+
+  let questions = [
+    {
+      id: 'like_apples',
+      text: 'Do you like apples?',
+      type: 'yesno'
+    },
+    {
+      id: 'bananas_instead',
+      text: 'Do you like bananas instead?',
+      type: 'yesno'
+    },
+    {
+      id: 'apple_colour',
+      text: 'Do you like *RED* apples?',
+      type: 'yesno'
+    },
+    {
+      id: 'red_apple_today',
+      text: 'Have you eaten a red apple today?',
+      type: 'yesno'
+    },
+    {
+      id: 'doctor_away',
+      text: 'Did it keep the doctor away?',
+      type: 'yesno'
+    }
+  ];
+
   describe('QuestionFields have correct initial state', function() {
     beforeEach(function () {
       this.wrapper =
