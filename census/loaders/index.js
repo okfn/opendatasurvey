46dca85642fc98480985fda9983b393f0cbcd757
Brook Elgie <specialbrew@gmail.com>
2016-10-07 11:13:54 +0100

    [#786] Refactor createData definition
    Not a fan of this _.chain and _.pairs/_.objects syntax. Prefer to be
    more explicit.

--- census/loaders/index.js
+++ census/loaders/index.js
@@ -61,18 +61,14 @@ let _createQuestionsForQuestionSet = function(questionsUrl,
     return Promise.all(
       _.map(data, dataObj => {
         // Allow custom data mapping
-        let createData = _.chain(controllerUtils.questionMapper(dataObj,
-                                                                qset.site))
+        let createData = controllerUtils.questionMapper(dataObj, qset.site);
         // All records belongs to certain domain
-        .extend({
+        createData = _.extend(createData, {
           site: qset.site,
           questionsetid: qset.id
-        })
-        .pairs()
+        });
         // User may mix up lower cased and upper cased field names
-        .map(P => [P[0].toLowerCase(), P[1]])
-        .object()
-        .value();
+        createData = _.mapKeys(createData, (v, key) => key.toLowerCase());
         return models.Question.create(createData, {transaction: transaction});
       })
     );
@@ -80,7 +76,7 @@ let _createQuestionsForQuestionSet = function(questionsUrl,
 };
 
 /*
-  A helper function to create a QuestionSet from the parsed quesiton set url,
+  A helper function to create a QuestionSet from the parsed question set url,
   and associate it with each dataset in the datasets array.
 */
 let _createQuestionSetForDatasets = function(datasets,
@@ -210,16 +206,12 @@ var loadData = function(options, models) {
     .spread((data, site) => {
       return Promise.all(_.map(data, dataObj => {
         // Allow custom data mapping
-        let createData = _.chain(
-          _.isFunction(options.mapper) ? options.mapper(dataObj, site) : dataObj
-        )
+        let createData = _.isFunction(options.mapper) ?
+          options.mapper(dataObj, site) : dataObj;
         // All records belongs to certain domain
-        .extend({site: options.site})
-        .pairs()
+        createData = _.extend(createData, {site: options.site});
         // User may mix up lower cased and upper cased field names
-        .map(P => [P[0].toLowerCase(), P[1]])
-        .object()
-        .value();
+        createData = _.mapKeys(createData, (v, key) => key.toLowerCase());
         return options.Model.create(createData, {transaction: t});
       }));
     });
