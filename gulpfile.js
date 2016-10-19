4c7c99a711ee5f775116ff73afa0b80afe466fbf
Yevgene Bogomolny <ybo@censhare.com>
2016-10-19 10:36:03 +0200

    Added SASS compiler to gulp Cleanup markup HTML Fixed small issues on page

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,17 @@ var gulp = require('gulp');
 var gulpConcatPo = require('gulp-concat-po');
 var gulpXgettext = require('gulp-xgettext');
 var gulpReplace = require('gulp-replace');
+var gulpRename = require('gulp-rename');
+var gulpSass = require('gulp-ruby-sass');
 var exec = require('child_process').exec;
 
-gulp.task('pot', function(){
+gulp.task('pot', pot);
+gulp.task('update-po', updatePo);
+// Prepare all /census/locale/*/LC_MESSAGES/*.po files to use with i18n-abide
+gulp.task('compile-po', compilePo);
+gulp.task('compile-styles', compileStyles);
+
+function pot() {
   return gulp.src('templates/**/*.html', {
     base: '.'
   }).pipe(gulpReplace(/or gettext/g, "|| gettext")).pipe(gulpXgettext({
@@ -15,15 +23,22 @@ gulp.task('pot', function(){
       name: 'format'
     }]),
     bin: 'node_modules/.bin/jsxgettext'
-  })).pipe(gulpConcatPo('messages.pot')).pipe(gulp.dest("locale/templates/LC_MESSAGES"));
-});
+  })).pipe(gulpConcatPo('messages.pot'))
+    .pipe(gulp.dest("locale/templates/LC_MESSAGES"));
+}
 
-gulp.task('update-po', function(){
+function updatePo() {
   return exec('./node_modules/.bin/merge-po locale');
-});
+}
 
-// Task to prepare all /census/locale/*/LC_MESSAGES/*.po files to
-// use with i18n-abide
-gulp.task('compile-po', function() {
-  return exec('./node_modules/.bin/compile-json ./census/locale ./census/locale');
-});
+function compilePo() {
+  return exec(
+    './node_modules/.bin/compile-json ./census/locale ./census/locale');
+}
+
+function compileStyles() {
+  return gulpSass(['census/static/scss/styles.scss'])
+    //.pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulpRename('styles.css'))
+    .pipe(gulp.dest('census/static/css'));
+}
