5761d2641dd623fe79c83c7d817134be33a9eb9d
Brook Elgie <specialbrew@gmail.com>
2016-10-25 12:43:19 +0100

    [#784] Design changes for entry form.
    This includes:
    - accommodation for `current` entry values (not yet implemented)
    - review and submission instruction box (#791)

--- census/ui_app/EntryForm.jsx
+++ census/ui_app/EntryForm.jsx
@@ -62,37 +62,44 @@ const EntryForm = React.createClass({
     </div>
 
     <div className="scale first question">
-      <div className="instructions"></div>
       <div className="main">
-        <h2><span>A1.</span> Rate your knowledge of <em>{ this.props.context.datasetName }</em>.</h2>
-        <div className="answer">
-          <span>
-            <input type="radio" name="yourKnowledgeDomain"
-                   id="yourKnowledgeDomain1"
-                   value="1"
-                   defaultChecked={this.props.answers.yourKnowledgeDomain === '1'} />
-            <label htmlFor="yourKnowledgeDomain1">
-              <span>1</span> <em className="description"> I'm not familiar at all with the field</em>
-            </label>
-          </span>
-          <span>
-            <input type="radio" name="yourKnowledgeDomain"
-                   id="yourKnowledgeDomain2"
-                   value="2"
-                   defaultChecked={this.props.answers.yourKnowledgeDomain === '2'} />
-            <label htmlFor="yourKnowledgeDomain2">
-              <span>2</span> <em className="description">I have some knowledge about the field</em>
-            </label>
-          </span>
-          <span>
-            <input type="radio" name="yourKnowledgeDomain"
-                   id="yourKnowledgeDomain3"
-                   value="3"
-                   defaultChecked={this.props.answers.yourKnowledgeDomain === '3'} />
-            <label htmlFor="yourKnowledgeDomain3">
-              <span>3</span> <em className="description">I have advanced knowledge in this field</em>
-            </label>
-          </span>
+        <div>
+          <div className="instructions"></div>
+          <h2><span>A1.</span> Rate your knowledge of <em>{ this.props.context.datasetName }</em>.</h2>
+        </div>
+        <div>
+          <div className="current"></div>
+          <div className="answer-wrapper">
+            <div className="answer">
+              <span>
+                <input type="radio" name="yourKnowledgeDomain"
+                       id="yourKnowledgeDomain1"
+                       value="1"
+                       defaultChecked={this.props.answers.yourKnowledgeDomain === '1'} />
+                <label htmlFor="yourKnowledgeDomain1">
+                  <span>1</span> <em className="description"> I'm not familiar at all with the field</em>
+                </label>
+              </span>
+              <span>
+                <input type="radio" name="yourKnowledgeDomain"
+                       id="yourKnowledgeDomain2"
+                       value="2"
+                       defaultChecked={this.props.answers.yourKnowledgeDomain === '2'} />
+                <label htmlFor="yourKnowledgeDomain2">
+                  <span>2</span> <em className="description">I have some knowledge about the field</em>
+                </label>
+              </span>
+              <span>
+                <input type="radio" name="yourKnowledgeDomain"
+                       id="yourKnowledgeDomain3"
+                       value="3"
+                       defaultChecked={this.props.answers.yourKnowledgeDomain === '3'} />
+                <label htmlFor="yourKnowledgeDomain3">
+                  <span>3</span> <em className="description">I have advanced knowledge in this field</em>
+                </label>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
       <div className="comments">
@@ -106,37 +113,44 @@ const EntryForm = React.createClass({
     </div>
 
     <div className="scale question">
-      <div className="instructions"></div>
       <div className="main">
-        <h2><span>A2.</span> Rate your knowledge of open data.</h2>
-        <div className="answer">
-          <span>
-            <input type="radio" name="yourKnowledgeOpenData"
-                   id="yourKnowledgeOpenData1"
-                   value="1"
-                   defaultChecked={this.props.answers.yourKnowledgeOpenData === '1'} />
-            <label htmlFor="yourKnowledgeOpenData1">
-              <span>1</span> <em className="description">I'm not familiar at all with open data</em>
-            </label>
-          </span>
-          <span>
-            <input type="radio" name="yourKnowledgeOpenData"
-                   id="yourKnowledgeOpenData2"
-                   value="2"
-                   defaultChecked={this.props.answers.yourKnowledgeOpenData === '2'} />
-            <label htmlFor="yourKnowledgeOpenData2">
-              <span>2</span> <em className="description">I have some knowledge about open data</em>
-            </label>
-          </span>
-          <span>
-            <input type="radio" name="yourKnowledgeOpenData"
-                   id="yourKnowledgeOpenData3"
-                   value="3"
-                   defaultChecked={this.props.answers.yourKnowledgeOpenData === '3'} />
-            <label htmlFor="yourKnowledgeOpenData3">
-              <span>3</span> <em className="description">I have advanced knowledge</em>
-            </label>
-          </span>
+        <div>
+          <div className="instructions"></div>
+          <h2><span>A2.</span> Rate your knowledge of open data.</h2>
+        </div>
+        <div>
+          <div className="current"></div>
+          <div className="answer-wrapper">
+            <div className="answer">
+              <span>
+                <input type="radio" name="yourKnowledgeOpenData"
+                       id="yourKnowledgeOpenData1"
+                       value="1"
+                       defaultChecked={this.props.answers.yourKnowledgeOpenData === '1'} />
+                <label htmlFor="yourKnowledgeOpenData1">
+                  <span>1</span> <em className="description">I'm not familiar at all with open data</em>
+                </label>
+              </span>
+              <span>
+                <input type="radio" name="yourKnowledgeOpenData"
+                       id="yourKnowledgeOpenData2"
+                       value="2"
+                       defaultChecked={this.props.answers.yourKnowledgeOpenData === '2'} />
+                <label htmlFor="yourKnowledgeOpenData2">
+                  <span>2</span> <em className="description">I have some knowledge about open data</em>
+                </label>
+              </span>
+              <span>
+                <input type="radio" name="yourKnowledgeOpenData"
+                       id="yourKnowledgeOpenData3"
+                       value="3"
+                       defaultChecked={this.props.answers.yourKnowledgeOpenData === '3'} />
+                <label htmlFor="yourKnowledgeOpenData3">
+                  <span>3</span> <em className="description">I have advanced knowledge</em>
+                </label>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
       <div className="comments">
@@ -171,40 +185,55 @@ const EntryForm = React.createClass({
     <div className="text question">
       <div className="instructions"></div>
       <div className="main">
-        <h2>Any other comments?</h2>
-        <div className="answer">
-          <textarea name="details" rows="5"
-                    defaultValue={this.props.answers.details}></textarea>
+        <div>
+          <div className="instructions"></div>
+          <h2>Any other comments?</h2>
+        </div>
+        <div>
+          <div className="current"></div>
+          <div className="answer-wrapper">
+            <div className="answer">
+              <textarea name="details" rows="5"
+                        defaultValue={this.props.answers.details}></textarea>
+            </div>
+          </div>
         </div>
       </div>
       <div className="comments"></div>
     </div>
 
     <div className="yes-no sub neutral question">
-      <div className="instructions">
-        <div className="collapse" id="instructionsAttribution">
-          <h4>Instructions</h4>
-          <p>By default, submissions to the census are credited to the submitter. If you would prefer to remain anonymous, please indicate so by checking the box.</p>
-        </div>
-        <a className="toggle" role="button" data-toggle="collapse" href="#instructionsAttribution" aria-expanded="false" aria-controls="instructionsAttribution"><span className="sr-only">Help</span><span className="icon">?</span></a>
-      </div>
       <div className="main">
-        <h2>Would you prefer your submission to remain anonymous?</h2>
-        <div className="answer">
-          <input type="radio" name="anonymous"
-                 id="anonymousNo"
-                 value="No"
-                 defaultChecked={this.props.answers.anonymous === 'No'} />
-          <label htmlFor="anonymousNo">
-            <span>No</span>
-          </label>
-          <input type="radio" name="anonymous"
-                 id="anonymousYes"
-                 value="Yes"
-                 defaultChecked={this.props.answers.anonymous === 'Yes'} />
-          <label htmlFor="anonymousYes">
-            <span>Yes</span>
-          </label>
+        <div>
+          <div className="instructions">
+            <div className="collapse" id="instructionsAttribution">
+              <h4>Instructions</h4>
+              <p>By default, submissions to the census are credited to the submitter. If you would prefer to remain anonymous, please indicate so by checking the box.</p>
+            </div>
+            <a className="toggle" role="button" data-toggle="collapse" href="#instructionsAttribution" aria-expanded="false" aria-controls="instructionsAttribution"><span className="sr-only">Help</span><span className="icon">?</span></a>
+          </div>
+          <h2>Would you prefer your submission to remain anonymous?</h2>
+        </div>
+        <div>
+          <div className="current"></div>
+          <div className="answer-wrapper">
+            <div className="answer">
+              <input type="radio" name="anonymous"
+                     id="anonymousNo"
+                     value="No"
+                     defaultChecked={this.props.answers.anonymous === 'No'} />
+              <label htmlFor="anonymousNo">
+                <span>No</span>
+              </label>
+              <input type="radio" name="anonymous"
+                     id="anonymousYes"
+                     value="Yes"
+                     defaultChecked={this.props.answers.anonymous === 'Yes'} />
+              <label htmlFor="anonymousYes">
+                <span>Yes</span>
+              </label>
+            </div>
+          </div>
         </div>
       </div>
       <div className="comments"></div>
--- census/ui_app/QuestionFields.jsx
+++ census/ui_app/QuestionFields.jsx
@@ -38,24 +38,32 @@ const baseQuestionField = QuestionField => {
 let QuestionFieldText = React.createClass({
   render() {
     return (<div className={'text question ' + this.props.getClassValues()}>
-      <helpers.QuestionInstructions instructionText={this.props.instructions}
-                            id={this.props.id} />
       <div className="main">
-        <helpers.QuestionHeader label={this.props.label}>
-          {this.props.children.toString()}
-        </helpers.QuestionHeader>
-        <div className="answer">
-          <input type="text" value={this.props.value}
-                             name={this.props.id}
-                             onChange={this.handler}
-                             disabled={!this.props.visibleProps.enabled} />
+        <div>
+          <helpers.QuestionInstructions instructionText={this.props.instructions}
+                                        id={this.props.id} />
+          <helpers.QuestionHeader label={this.props.label}>
+            {this.props.children.toString()}
+          </helpers.QuestionHeader>
+        </div>
+        <div>
+          <div className="current"></div>
+          <div className="answer-wrapper">
+            <div className="answer">
+              <input type="text"
+                     value={this.props.value}
+                     name={this.props.id}
+                     onChange={this.handler}
+                     disabled={!this.props.visibleProps.enabled} />
+            </div>
+          </div>
         </div>
       </div>
       <helpers.QuestionComments id={this.props.id}
-                        placeholder={this.props.placeholder}
-                        commentValue={this.props.commentValue}
-                        onCommentChange={this.props.onCommentChange}
-                        disabled={!this.props.visibleProps.enabled} />
+                                placeholder={this.props.placeholder}
+                                commentValue={this.props.commentValue}
+                                onCommentChange={this.props.onCommentChange}
+                                disabled={!this.props.visibleProps.enabled} />
     </div>);
   },
 
@@ -68,33 +76,40 @@ QuestionFieldText = baseQuestionField(QuestionFieldText);
 let QuestionFieldYesNo = React.createClass({
   render() {
     return (<div className={'yes-no question ' + this.props.getClassValues()}>
-      <helpers.QuestionInstructions instructionText={this.props.instructions}
-                            id={this.props.id} />
       <div className="main">
-        <helpers.QuestionHeader label={this.props.label}>
-          {this.props.children.toString()}
-        </helpers.QuestionHeader>
-        <div className="answer">
-          <input type="radio"
-                 name={this.props.id}
-                 id={this.props.id + '1'}
-                 value="No"
-                 checked={(this.props.value === 'No')}
-                 disabled={!this.props.visibleProps.enabled}
-                 onChange={this.handler} />
-          <label htmlFor={this.props.id + '1'}>
-            <span>No</span>
-          </label>
-          <input type="radio"
-                 name={this.props.id}
-                 id={this.props.id + '2'}
-                 value="Yes"
-                 checked={(this.props.value === 'Yes')}
-                 disabled={!this.props.visibleProps.enabled}
-                 onChange={this.handler} />
-          <label htmlFor={this.props.id + '2'}>
-            <span>Yes</span>
-          </label>
+        <div>
+          <helpers.QuestionInstructions instructionText={this.props.instructions}
+                                        id={this.props.id} />
+          <helpers.QuestionHeader label={this.props.label}>
+            {this.props.children.toString()}
+          </helpers.QuestionHeader>
+        </div>
+        <div>
+          <div className="current"></div>
+          <div className="answer-wrapper">
+            <div className="answer">
+              <input type="radio"
+                     name={this.props.id}
+                     id={this.props.id + '1'}
+                     value="No"
+                     checked={(this.props.value === 'No')}
+                     disabled={!this.props.visibleProps.enabled}
+                     onChange={this.handler} />
+              <label htmlFor={this.props.id + '1'}>
+                <span>No</span>
+              </label>
+              <input type="radio"
+                     name={this.props.id}
+                     id={this.props.id + '2'}
+                     value="Yes"
+                     checked={(this.props.value === 'Yes')}
+                     disabled={!this.props.visibleProps.enabled}
+                     onChange={this.handler} />
+              <label htmlFor={this.props.id + '2'}>
+                <span>Yes</span>
+              </label>
+            </div>
+          </div>
         </div>
       </div>
       <helpers.QuestionComments id={this.props.id}
@@ -141,14 +156,21 @@ let QuestionFieldLikert = React.createClass({
                                         disabled={!this.props.visibleProps.enabled} />;
     });
     return (<div className={'scale question ' + this.props.getClassValues()}>
-      <helpers.QuestionInstructions instructionText={this.props.instructions}
-                                    id={this.props.id} />
       <div className="main">
-        <helpers.QuestionHeader label={this.props.label}>
-          {this.props.children.toString()}
-        </helpers.QuestionHeader>
-        <div className="answer">
-          {scaleOptionNodes}
+        <div>
+          <helpers.QuestionInstructions instructionText={this.props.instructions}
+                                        id={this.props.id} />
+          <helpers.QuestionHeader label={this.props.label}>
+            {this.props.children.toString()}
+          </helpers.QuestionHeader>
+        </div>
+        <div>
+          <div className="current"></div>
+          <div className="answer-wrapper">
+            <div className="answer">
+              {scaleOptionNodes}
+            </div>
+          </div>
         </div>
       </div>
       <helpers.QuestionComments id={this.props.id}
@@ -218,14 +240,21 @@ let QuestionFieldSource = React.createClass({
       sourceLines.push(node);
     }
     return (<div className={'source question ' + this.props.getClassValues()}>
-      <helpers.QuestionInstructions instructionText={this.props.instructions}
-                            id={this.props.id} />
       <div className="main">
-        <helpers.QuestionHeader label={this.props.label}>
-          {this.props.children.toString()}
-        </helpers.QuestionHeader>
-        <div className="answer">
-          {sourceLines}
+        <div>
+          <helpers.QuestionInstructions instructionText={this.props.instructions}
+                                        id={this.props.id} />
+          <helpers.QuestionHeader label={this.props.label}>
+            {this.props.children.toString()}
+          </helpers.QuestionHeader>
+        </div>
+        <div>
+          <div className="current"></div>
+          <div className="answer-wrapper">
+            <div className="answer">
+              {sourceLines}
+            </div>
+          </div>
         </div>
       </div>
       <helpers.QuestionComments id={this.props.id}
@@ -325,20 +354,27 @@ let QuestionFieldMultipleChoice = React.createClass({
               </QuestionFieldMultipleChoiceOption>;
     });
     return (<div className={'multiple question ' + this.props.getClassValues()}>
-      <helpers.QuestionInstructions instructionText={this.props.instructions}
-                                    id={this.props.id} />
       <div className="main">
-        <helpers.QuestionHeader label={this.props.label}>
-          {this.props.children.toString()}
-        </helpers.QuestionHeader>
-        <div className="answer">
-          <ul>
-            {choices}
-          </ul>
+        <div>
+          <helpers.QuestionInstructions instructionText={this.props.instructions}
+                                        id={this.props.id} />
+          <helpers.QuestionHeader label={this.props.label}>
+            {this.props.children.toString()}
+          </helpers.QuestionHeader>
         </div>
-        <QuestionFieldMultipleChoiceOther includeOther={this.props.config.includeOther}
-                                          id={this.props.id + '_other'}
-                                          disabled={!this.props.visibleProps.enabled} />
+        <div>
+          <div className="current"></div>
+          <div className="answer-wrapper">
+            <div className="answer">
+              <ul>
+                {choices}
+              </ul>
+            </div>
+            <QuestionFieldMultipleChoiceOther includeOther={this.props.config.includeOther}
+                                              id={this.props.id + '_other'}
+                                              disabled={!this.props.visibleProps.enabled} />
+          </div>
+        </div>
       </div>
       <helpers.QuestionComments id={this.props.id}
                                 placeholder={this.props.placeholder}
