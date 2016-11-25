d31d2e9218524c2d2adb18bc31c24620279f10c5
Brook Elgie <specialbrew@gmail.com>
2016-11-25 16:02:07 +0000

    [#829] Add validator for Source question
    This special rule checks that each Source URL is a valid URL for the
    location quesiton which uses `QuestionFieldSource` question type.

--- census/ui_app/validators.js
+++ census/ui_app/validators.js
@@ -12,6 +12,17 @@ let validators = {
       return !validator.isEmpty(value.toString());
     },
     message: 'Question is required'
+  },
+  sourceIsURL: {
+    rule: value => {
+      for (let i = 0; i < value.length; i++) {
+        if (!validator.isURL(value[i].urlValue)) {
+          return false;
+        }
+      }
+      return true;
+    },
+    message: 'Each source URL must be a valid URL'
   }
 };
 
